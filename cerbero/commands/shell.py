e56dcf595a646d03dbd9d49ab27a37adfe87f3e5
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-02-23 12:37:25 +0000

    Add shell command

---
+++ cerbero/commands/shell.py
@@ -0,0 +1,36 @@
+# cerbero - a multi-platform build system for Open Source software
+# Copyright (C) 2012 Andoni Morales Alastruey <ylatuya@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import os
+
+from cerbero.commands import Command, register_command
+from cerbero.utils import N_
+
+
+class Shell(Command):
+    doc = N_('Starts a shell with the build environment')
+    name = 'shell'
+
+    def __init__(self):
+        Command.__init__(self, [])
+
+    def run(self, config, args):
+        shell = os.environ.get('SHELL', '/bin/sh')
+        os.execlp(shell, shell)
+
+register_command(Shell)
