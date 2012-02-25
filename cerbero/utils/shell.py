6c77f4fb46cb87511674dafc8f555d28dd5372e9
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-02-25 20:03:51 +0100

    Document shell.py and fix pep8

--- cerbero/utils/shell.py
+++ cerbero/utils/shell.py
@@ -32,42 +32,69 @@ PATCH = 'patch'
 TAR = 'tar'
 
 
-def call (cmd, cmd_dir='.', fail=True):
+def call(cmd, cmd_dir='.', fail=True):
+    '''
+    Run a shell command
+
+    @param cmd: the command to run
+    @type cmd: str
+    @param cmd_dir: directory where the command will be run
+    @param cmd_dir: str
+    @param fail: wheter to raise an exception if the command failed or not
+    @type fail: bool
+    '''
     try:
-        logging.info ("Running command '%s'" % cmd)
+        logging.info("Running command '%s'" % cmd)
         ret = subprocess.check_call(cmd, cwd=cmd_dir,
                                     stderr=subprocess.STDOUT,
                                     stdout=sys.stdout, env=os.environ.copy(),
                                     shell=True)
     except Exception, ex:
         if fail:
-            raise FatalError (_("Error running command %s: %s") % (cmd, ex))
+            raise FatalError(_("Error running command %s: %s") % (cmd, ex))
         else:
             ret = 0
     return ret
 
 
-def check_call (cmd, cmd_dir):
+def check_call(cmd, cmd_dir):
     try:
-        ret = subprocess.check_output (shlex.split(cmd), cwd=cmd_dir)
+        ret = subprocess.check_output(shlex.split(cmd), cwd=cmd_dir)
     except Exception, ex:
-        raise FatalError (_("Error running command %s: %s") % (cmd, ex))
+        raise FatalError(_("Error running command %s: %s") % (cmd, ex))
     return ret
 
 
 def apply_patch(patch, directory, strip=1):
+    '''
+    Apply a patch
+
+    @param patch: path of the patch file
+    @type patch: str
+    @param directory: directory to apply the apply
+    @type: directory: str
+    @param strip: strip
+    @type strip: int
+    '''
     logging.info("Applying patch %s" % (patch))
-    call ('%s -p%s -f -i %s', (PATCH, strip, patch))
+    call('%s -p%s -f -i %s', (PATCH, strip, patch))
 
 
-def unpack(filename, dest):
-    logging.info("Unpacking %s in %s" % (filename, dest))
-    if filename.endswith('tar.gz') or filename.endswith('tar.bz2'):
-        tf = tarfile.open(filename, mode='r:*')
-        tf.extractall(path=dest)
-    if filename.endswith('tar.xz'):
-        call ("%s -Jxf %s" % (TAR, filename), dest)
-    if filename.endswith('.zip'):
-        zf = zipfile.ZipFile(filename, "r")
-        zf.extractall(path=dest)
+def unpack(filepath, output_dir):
+    '''
+    Extracts a tarball
 
+    @param filepath: path of the tarball
+    @type filepath: str
+    @param output_dir: output directory
+    @type output_dir: str
+    '''
+    logging.info("Unpacking %s in %s" % (filepath, output_dir))
+    if filepath.endswith('tar.gz') or filepath.endswith('tar.bz2'):
+        tf = tarfile.open(filepath, mode='r:*')
+        tf.extractall(path=output_dir)
+    if filepath.endswith('tar.xz'):
+        call("%s -Jxf %s" % (TAR, filepath), output_dir)
+    if filepath.endswith('.zip'):
+        zf = zipfile.ZipFile(filepath, "r")
+        zf.extractall(path=output_dir)
