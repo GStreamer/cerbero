f59ad0f85079d06e2bcc68dcec7563a656fcfa7f
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-03-09 12:52:43 +0100

    Copy subprocess.check_output() from python 2.7 here
    To be able to use cerbero on older distros like squeeze

--- cerbero/utils/shell.py
+++ cerbero/utils/shell.py
@@ -60,7 +60,7 @@ def call(cmd, cmd_dir='.', fail=True):
 
 def check_call(cmd, cmd_dir=None):
     try:
-        ret = subprocess.check_output(shlex.split(cmd), cwd=cmd_dir)
+        ret = check_output(shlex.split(cmd), cwd=cmd_dir)
     except Exception, ex:
         raise FatalError(_("Error running command %s: %s") % (cmd, ex))
     return ret
@@ -151,4 +151,37 @@ def recursive_download(url, destination):
         if not os.path.exists(destination):
             os.makedirs(destination)
         call("wget -i %s %s" % (f.name, url), destination)
+
+# Copied from python 2.7
+def check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte string.
+
+    If the exit code was non-zero it raises a CalledProcessError.  The
+    CalledProcessError object will have the return code in the returncode
+    attribute and output in the output attribute.
+
+    The arguments are the same as for the Popen constructor.  Example:
+
+    >>> check_output(["ls", "-l", "/dev/null"])
+    'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+    The stdout argument is not allowed as it is used internally.
+    To capture standard error in the result, use stderr=STDOUT.
+
+    >>> check_output(["/bin/sh", "-c",
+    ...               "ls -l non_existent_file ; exit 0"],
+    ...              stderr=STDOUT)
+    'ls: non_existent_file: No such file or directory\n'
+    """
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise CalledProcessError(retcode, cmd, output=output)
+    return output
 
