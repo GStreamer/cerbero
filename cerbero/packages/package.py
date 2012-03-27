c3f77eaf34f8d7e4da94b5b910eae999f8178af9
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-03-27 17:12:23 +0200

    List files in recipes

--- cerbero/packages/package.py
+++ cerbero/packages/package.py
@@ -59,97 +59,34 @@ class Package(PackageBase):
     Describes a set of files to produce disctribution packages for the different
     target platforms
 
-    @cvar libraries: list of libraries
-    @type libraries: list
-    @cvar platform_libs: list of platform dependant libraries
-    @type platform_libs: dict
-    @cvar binaries: list of binaries
-    @type binaries: list
-    @cvar platform_bins: list of platform dependant binaries
-    @type platform_bins: dict
-    @cvar files: list of files included in this package
-    @type type: files
-    @cvar platform_files: list of files included for a specific platform
-    @type type: dict
     @cvar deps: list of packages dependencies
     @type deps: list
-
-    @ivar extensions: file extensions for binaries and shared libraries
-    @type bext: dict
+    @cvar files: list of files included in this package
+    @type files: list
     '''
 
-    libraries = list()
-    platform_libs = {}
-    binaries = list()
-    platform_bins = {}
-    files = list()
-    platform_files = {}
     deps = list()
 
-    EXTENSIONS = {
-        Platform.WINDOWS: {'bext': '.exe', 'sext': '.dll', 'sdir': 'bin'},
-        Platform.LINUX: {'bext': '', 'sext': '.so', 'sdir': 'lib'},
-        Platform.DARWIN: {'bext': '', 'sext': '.dylib', 'sdir': 'lib'}}
+    def __init__(self, config, cookbook):
+        self.cookbook = cookbook
+        self._files.extend = self.platform_files.get(config.target_platform, [])
+        self._parse_files()
 
-    def __init__(self, config):
-        self.config = config
-        self.extensions = self.EXTENSIONS[self.config.target_platform]
-
-    def get_files_list(self):
+    def recipes_dependencies(self):
+        return [x.split(':')[0] for x in self._files]
+    
+    def files_list(self):
         files = []
-        files.extend(self._get_files())
-        files.extend(self._get_binaries())
-        files.extend(self._get_libraries())
-        return sorted(files)
-
-    def _get_files(self):
-        # Fille the list of regular files
-        files = []
-        files.extend(self.files)
-        if self.config.target_platform in self.platform_files:
-            files.extend(self.platform_files[self.config.target_platform])
-        return [f % self.extensions for f in files]
-
-    def _get_binaries(self):
-        files = []
-        # Fill the list of binaries
-        binaries = []
-        binaries.extend(self.binaries)
-        if self.config.target_platform in self.platform_bins:
-            binaries.extend(self.platform_bins[self.config.target_platform])
-        for f in binaries:
-            self.extensions['file'] = f
-            files.append('bin/%(file)s%(bext)s' % self.extensions)
+        for recipe, categories in self._recipes_files:
+            rfiles = self.cookbook.get_recipe(recipe).files_list(categories)
+            files.extend(rfiles)
         return files
 
-    def _get_libraries(self):
-        # Fill the list of binaries
-        # Unfortunately the filename might vary depending on the platform and we
-        # need to match the library name and it's extension with the list of
-        # files in the prefix
-        libraries = []
-        libraries.extend(self.libraries)
-        if self.config.target_platform in self.platform_libs:
-            libraries.extend(self.platform_libs[self.config.target_platform])
-        if len(libraries) == 0:
-            return []
-
-        pattern = '%(sdir)s/%(file)s*%(sext)s'
-        if self.config.target_platform == Platform.LINUX:
-            # libfoo.so.X, libfoo.so.X.Y.Z
-            pattern += '.*'
-
-        libsmatch = []
-        for f in libraries:
-            self.extensions['file'] = f
-            libsmatch.append(pattern % self.extensions)
-        # FIXME: I think's that's the fastest way of getting the list of
-        # libraries that matches the library name
-        sfiles = shell.check_call('ls %s' % ' '.join(libsmatch),
-                                 self.config.prefix, True, False, False).split('\n')
-        sfiles.remove('')
-        # remove duplicates
-        return list(set(sfiles))
+    def _parse_files(self):
+        self._recipes_files = {}
+        for r in self.files:
+            l = r.split(':')
+            self._recipes_files[l[0]] = l[1:]
 
 
 class MetaPackage(PackageBase):
@@ -160,12 +97,9 @@ class MetaPackage(PackageBase):
     @type packages: list
     @cvar icon: filename of the package icon
     @type icon: str
-    @cvar install_dir: dictionary with the installation paths for all platforms
-    @type install_dir: str
     '''
 
     icon = None
-    install_dir = {}
     packages = []
 
     def __init__(self, config):
