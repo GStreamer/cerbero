4ca14e9fb9c91ce6cf0d36865caddbf218c8d54f
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-03-29 01:19:43 +0200

    Init the FileProvider base class

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -100,6 +100,7 @@ class Recipe(FilesProvider):
         self.build_dir = os.path.abspath(self.build_dir)
         self.deps = self.deps or []
         self.platform_deps = self.platform_deps or []
+        FilesProvider.__init__(self, config)
         try:
             self.stype.__init__(self)
             self.btype.__init__(self)
