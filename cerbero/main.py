e94be06b1e96f42ce15d0a42977a6529d2b88ec9
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-05-28 09:51:59 -0700

    Revert "main: add print to help installing argparse"
    This reverts commit 1be3d68c86227764916d05226600e9e54cc37a8a.

--- cerbero/main.py
+++ cerbero/main.py
@@ -18,15 +18,9 @@
 
 from cerbero import hacks
 
-try:
-    import argparse
-except ImportError, e:
-    print "Could not import argparse. Try installing it with "\
-          "'sudo easy_install argparse"
-    raise e
-
 import sys
 import errno
+import argparse
 import logging
 import traceback
 
