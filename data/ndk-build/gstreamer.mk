d3deab6332fd625b9bde3bcc8cc01f2597096e76
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-11-02 13:00:29 +0100

    ndk-build: fix plugin link for mpegdemux2

--- data/ndk-build/gstreamer.mk
+++ data/ndk-build/gstreamer.mk
@@ -125,6 +125,7 @@ GSTREAMER_PLUGINS_LIBS       := $(call fix-plugin-name,gstsiren,siren)
 GSTREAMER_PLUGINS_LIBS       := $(call fix-plugin-name,sdp,sdpelem)
 GSTREAMER_PLUGINS_LIBS       := $(call fix-plugin-name,gstrtpmanager,rtpmanager)
 GSTREAMER_PLUGINS_LIBS       := $(call fix-plugin-name,scaletempo,scaletempoplugin)
+GSTREAMER_PLUGINS_LIBS       := $(call fix-plugin-name,mpegdemux2,mpegdemux)
 GSTREAMER_PLUGINS_LIBS       := $(subst gstgnonlin,gnl,$(GSTREAMER_PLUGINS_LIBS))
 
 # Generate the plugins' declaration strings
