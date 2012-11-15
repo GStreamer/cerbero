31b0bbbd76f56965d8754eb1781aa284e6029367
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-11-15 14:50:20 +0100

    bootstrap: only include glib-networking where available

--- cerbero/bootstrap/linux.py
+++ cerbero/bootstrap/linux.py
@@ -63,6 +63,9 @@ class DebianBootstraper (UnixBootstraper):
         UnixBootstraper.__init__(self, config)
         if self.config.arch == Architecture.X86_64:
             self.packages.append('ia32-libs')
+        if self.config.distro_version in [DistroVersion.DEBIAN_SQUEEZE,
+                DistroVersion.UBUNTU_MAVERICK, DistroVersion.UBUNTU_LUCID]:
+            self.packages.remove('glib-networking')
 
 
 
