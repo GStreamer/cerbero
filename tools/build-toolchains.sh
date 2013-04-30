c042f468fc2350b939b7bdc99d16d6b7b49ca4f0
Andoni Morales Alastruey <ylatuya@gmail.com>
2013-04-30 12:23:11 +0200

    toolchain: add paramater to wipe the builds

--- tools/build-toolchains.sh
+++ tools/build-toolchains.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
 # Build the Windows cross and native toolchains for x86 and x86_64
 
+WIPE=$1
+
 for a in "w64" "w32"
 do
   for p in "lin" "win"
   do
-    ./cerbero-uninstalled -c config/mingw-$a-$p.cbc wipe --force
+      echo "Building $p-$a toolchain"
+      if test "x$WIPE" = "x1"; then
+        ./cerbero-uninstalled -c config/mingw-$a-$p.cbc wipe --force
+      fi
     ./cerbero-uninstalled -c config/mingw-$a-$p.cbc build toolchain
   done
 done
