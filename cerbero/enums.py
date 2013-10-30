90a69ebdfc9fe45751c4764b9859137c02719515
Josep Torra <josep@fluendo.com>
2013-10-30 17:48:02 +0100

    Add support for Ubuntu saucy

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -59,6 +59,7 @@ class DistroVersion:
     UBUNTU_PRECISE = 'ubuntu_precise'
     UBUNTU_QUANTAL = 'ubuntu_quantal'
     UBUNTU_RARING = 'ubuntu_raring'
+    UBUNTU_SAUCY = 'ubuntu_saucy'
     FEDORA_16 = 'fedora_16'
     FEDORA_17 = 'fedora_17'
     FEDORA_18 = 'fedora_18'
