558e7cf42142175c3ce8e0b2da2f2562b65cc9a2
Andoni Morales Alastruey <ylatuya@gmail.com>
2013-11-28 11:24:37 +0100

    fetch: reset recipe status when the version changed
    This is usefull for git or svn repositories using master or HEAD
    to detect when upstream version changed and trigger a rebuild

--- cerbero/commands/fetch.py
+++ cerbero/commands/fetch.py
@@ -32,6 +32,11 @@ class Fetch(Command):
                 ArgparseArgument('recipes', nargs='*',
                     help=_('list of the recipes to fetch (fetch all if none '
                            'is passed)')),
+                ArgparseArgument('--no-deps', action='store_true',
+                    default=False, help=_('do not fetch dependencies')),
+                ArgparseArgument('--reset-rdeps', action='store_true',
+                    default=False, help=_('reset the status of reverse '
+                    'dependencies too')),
                 ]
             Command.__init__(self, args)
 
@@ -40,16 +45,34 @@ class Fetch(Command):
         fetch_recipes = []
         if not args.recipes:
             fetch_recipes = cookbook.get_recipes_list()
+        elif args.no_deps:
+            fetch_recipes = [cookbook.get_recipe(x) for x in args.recipes]
         else:
             for recipe in args.recipes:
                 fetch_recipes += cookbook.list_recipe_deps(recipe)
             fetch_recipes = remove_list_duplicates (fetch_recipes)
         m.message(_("Fetching the following recipes: %s") %
                   ' '.join([x.name for x in fetch_recipes]))
+        to_rebuild = []
         for i in range(len(fetch_recipes)):
             recipe = fetch_recipes[i]
             m.build_step(i + 1, len(fetch_recipes), recipe, 'Fetch')
             recipe.fetch()
+            bv = cookbook.recipe_built_version(recipe.name)
+            cv = recipe.built_version()
+            print bv, cv
+            if not cookbook.recipe_needs_build(recipe.name) and bv != cv:
+                to_rebuild.append(recipe)
+                cookbook.reset_recipe_status(recipe.name)
+                if args.reset_rdeps:
+                     for r in cookbook.list_recipe_reverse_deps(recipe.name):
+                        to_rebuild.append(r)
+                        cookbook.reset_recipe_status(r.name)
+
+        if to_rebuild:
+            m.message(_("These recipes have been updated and will "
+                        "be rebuilt:\n%s") %
+                        '\n'.join([x.name for x in to_rebuild]))
 
 
 register_command(Fetch)
