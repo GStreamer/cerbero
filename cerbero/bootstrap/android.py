5ceff4a171c72c0c3a41bc3e8eb3e745c1339a8d
Jan Schmidt <thaytan@noraisin.net>
2014-02-13 00:21:48 +1100

    android: Switch to NDK r9c.
    Fixes bug 724177 - android-x86 build issue with zlib.

--- cerbero/bootstrap/android.py
+++ cerbero/bootstrap/android.py
@@ -27,7 +27,7 @@ from cerbero.utils import shell
 class AndroidBootstraper (BootstraperBase):
 
     NDK_BASE_URL = 'http://dl.google.com/android/ndk/'
-    NDK_TAR = 'android-ndk-r9-linux-%s.tar.bz2'
+    NDK_TAR = 'android-ndk-r9c-linux-%s.tar.bz2'
 
     def start(self):
         dest = self.config.toolchain_prefix
@@ -41,7 +41,7 @@ class AndroidBootstraper (BootstraperBase):
         if not os.path.exists(os.path.join(dest, "README.TXT")):
             try:
                 shell.call('tar -xvjf %s' % ndk_tar, dest)
-                shell.call('mv android-ndk-r9/* .', dest)
+                shell.call('mv android-ndk-r9c/* .', dest)
             except Exception:
                 pass
 
