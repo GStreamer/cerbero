dc3fe59cc840d418a4d1a6497ca44a02f2cc5c17
Edward Hervey <bilboed@bilboed.com>
2014-05-22 10:43:36 +0200

    build-tools: Only require app-image-kit when *targetting* Linux

--- cerbero/bootstrap/build_tools.py
+++ cerbero/bootstrap/build_tools.py
@@ -56,6 +56,7 @@ class BuildTools (BootstrapperBase):
                 self.BUILD_TOOLS.append('yasm')
             if self.config.distro_version in [DistroVersion.REDHAT_6]:
                 self.BUILD_TOOLS.append('cmake')
+        if self.config.target_platform == Platform.LINUX:
             self.BUILD_TOOLS.append('app-image-kit')
         if self.config.target_platform == Platform.IOS:
             self.BUILD_TOOLS.append('gas-preprocessor')
