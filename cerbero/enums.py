db4ba3545c9408c113f83e0ea33b020580257d28
Sebastian Dröge <sebastian@centricular.com>
2014-10-09 10:28:52 +0300

    osx: Add support for yosemite

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -86,6 +86,7 @@ class DistroVersion:
     OS_X_MOUNTAIN_LION = 'osx_mountain_lion'
     OS_X_LION = 'osx_lion'
     OS_X_SNOW_LEOPARD = 'osx_snow_leopard'
+    OS_X_YOSEMITE = 'osx_yosemite'
     IOS_6_0 = 'ios_6_0'
     IOS_6_1 = 'ios_6_1'
     IOS_7_0 = 'ios_7_0'
