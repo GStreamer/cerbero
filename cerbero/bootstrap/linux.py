4724d5871d3d2ddacc409f400eea7ea30e2b79ef
Nirbheek Chauhan <nirbheek@centricular.com>
2015-03-02 21:38:10 +0530

    harfbuzz: Unconditionally disable ICU support
    Not really useful for us, and ICU isn't available on all distros anyway

--- cerbero/bootstrap/linux.py
+++ cerbero/bootstrap/linux.py
@@ -50,8 +50,7 @@ class DebianBootstrapper (UnixBootstrapper):
                 'libxml-simple-perl', 'dpkg-dev', 'debhelper',
                 'build-essential', 'devscripts', 'fakeroot', 'transfig',
                 'gperf', 'libdbus-glib-1-dev', 'wget', 'glib-networking',
-                'libxtst-dev', 'libxrandr-dev',
-                'libglu1-mesa-dev', 'icu-devtools', 'libicu-dev']
+                'libxtst-dev', 'libxrandr-dev', 'libglu1-mesa-dev']
     distro_packages = {
         DistroVersion.DEBIAN_SQUEEZE: ['libgtk2.0-dev'],
         DistroVersion.UBUNTU_MAVERICK: ['libgtk2.0-dev'],
