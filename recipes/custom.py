86b77e42cb0b006a13ea1307728a6ca88876f4bb
Jan Schmidt <jan@centricular.com>
2015-09-21 19:26:34 +1000

    Add gst-editing-services to the list of plugin packages
    Add gst-editing-services to the list of packages that might
    provide plugins so that it gets scanned and appropriate
    static plugin directives are generated for Android and iOS
    
    https://bugzilla.gnome.org/show_bug.cgi?id=755310

--- recipes/custom.py
+++ recipes/custom.py
@@ -130,7 +130,7 @@ def list_gstreamer_1_0_plugins_by_category(config):
         plugins = defaultdict(list)
         for r in ['gstreamer-1.0', 'gst-plugins-base-1.0', 'gst-plugins-good-1.0',
                   'gst-plugins-bad-1.0', 'gst-plugins-ugly-1.0',
-                  'gst-libav-1.0']:
+                  'gst-libav-1.0', 'gst-editing-services-1.0']:
             r = cookbook.get_recipe(r)
             for attr_name in dir(r):
                 if attr_name.startswith('files_plugins_'):
