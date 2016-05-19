daea5005f68ee3ccbcb9e28d39036d24afb201f8
Nirbheek Chauhan <nirbheek@centricular.com>
2016-05-19 11:31:05 +0530

    cerbero hacks: Don't override shutil.rmtree with `rm`
    `rm` is massively slower than the native Python implementation on
    Windows, so we try to workaround the most common issue (readonly files
    cannot be deleted), and if any more issues crop up, we just delete that
    path with `rm`.

--- cerbero/hacks.py
+++ cerbero/hacks.py
@@ -126,18 +126,41 @@ if sys.platform.startswith('win'):
     os.path.abspath = abspath
     os.path.realpath = realpath
 
-# rmtree fails to often with access denied
+import stat
 import shutil
-from cerbero.utils.shell import call
+from shutil import rmtree as shutil_rmtree
+from cerbero.utils.shell import call as shell_call
 
-
-def rmtree(path, onerror=None):
-    call('rm -rf %s' % path)
-
+def rmtree(path, ignore_errors=False, onerror=None):
+    '''
+    shutil.rmtree often fails with access denied. On Windows this happens when
+    a file is readonly. On Linux this can happen when a directory doesn't have
+    the appropriate permissions (Ex: chmod 200) and many other cases.
+    '''
+    def force_removal(func, path, excinfo):
+        '''
+        This is the only way to ensure that readonly files are deleted by
+        rmtree on Windows. See: http://bugs.python.org/issue19643
+        '''
+        os.chmod(path, stat.S_IWRITE)
+        # Due to the way 'onerror' is implemented in shutil.rmtree, errors
+        # encountered while listing directories cannot be recovered from. So if
+        # a directory cannot be listed, shutil.rmtree assumes that it is empty
+        # and it tries to call os.remove() on it which fails. This is just one
+        # way in which this can fail, so for robustness we just call 'rm' if we
+        # get an OSError while trying to remove a specific path.
+        # See: http://bugs.python.org/issue8523
+        try:
+            func(path)
+        except OSError:
+            shell_call('rm -rf ' + path)
+    # We try to not use `rm` because on Windows because it's about 20-30x slower
+    if not onerror:
+        shutil_rmtree(path, ignore_errors, onerror=force_removal)
+    else:
+        shutil_rmtree(path, ignore_errors, onerror)
 
 shutil.rmtree = rmtree
-
-
 
 ### OS X Hacks ###
 
