f2aa053eb64098079c97e3c8dd2b2a1522907351
Sebastian Dröge <sebastian@centricular.com>
2016-07-04 10:18:49 +0200

    android: Instead of cat | sed, use sed directly on the file
    Windows has no cat so it doesn't work. We lose some symmetry in the code in
    exchange for Windows portability.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=768326

--- data/ndk-build/gstreamer-1.0.mk
+++ data/ndk-build/gstreamer-1.0.mk
@@ -207,8 +207,7 @@ genstatic_$(TARGET_ARCH_ABI): PRIV_G_R := $(G_IO_MODULES_DECLARE)
 genstatic_$(TARGET_ARCH_ABI):
 	@$(HOST_ECHO) "GStreamer      : [GEN] => $(PRIV_C)"
 	@$(call host-mkdir,$(PRIV_B_DIR))
-	@cat $(PRIV_C_IN) | \
-		$(SED) "s/@PLUGINS_DECLARATION@/$(PRIV_P_D)/g" | \
+	@$(SED) "s/@PLUGINS_DECLARATION@/$(PRIV_P_D)/g" $(PRIV_C_IN) | \
 		$(SED) "s/@PLUGINS_REGISTRATION@/$(PRIV_P_R)/g" | \
 		$(SED) "s/@G_IO_MODULES_LOAD@/$(PRIV_G_L)/g" | \
 		$(SED) "s/@G_IO_MODULES_DECLARE@/$(PRIV_G_R)/g" > $(PRIV_C)
@@ -254,8 +253,7 @@ copyjavasource_$(TARGET_ARCH_ABI):
 	@$(foreach file,$(GSTREAMER_PLUGINS_CLASSES), \
 		$(call host-cp,$(GSTREAMER_NDK_BUILD_PATH)$(file),$(GSTREAMER_JAVA_SRC_DIR)/org/freedesktop/gstreamer/$(file)) && ) echo Done cp
 
-	@cat $(GSTREAMER_NDK_BUILD_PATH)/GStreamer.java | \
-		$(SED) "s;@INCLUDE_FONTS@;$(GSTREAMER_INCLUDE_FONTS_SUBST);g" | \
+	@$(SED) "s;@INCLUDE_FONTS@;$(GSTREAMER_INCLUDE_FONTS_SUBST);g" $(GSTREAMER_NDK_BUILD_PATH)/GStreamer.java | \
 		$(SED) "s;@INCLUDE_CA_CERTIFICATES@;$(GSTREAMER_INCLUDE_CA_CERTIFICATES_SUBST);g" | \
 		$(SED) "s;@INCLUDE_COPY_FILE@;$(GSTREAMER_COPY_FILE_SUBST);g" \
 		> $(GSTREAMER_JAVA_SRC_DIR)/org/freedesktop/gstreamer/GStreamer.java
