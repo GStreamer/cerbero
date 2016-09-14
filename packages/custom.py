e5630d47edfff26ec6b1a36a9234d7c9a89dc5b9
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2016-09-14 09:28:40 -0400

    packages: Centralize GStreamer package common values
    This reduces the redundancy and quickly allow changing the
    version.

---
+++ packages/custom.py
@@ -0,0 +1,13 @@
+# -*- Mode: Python -*- vi:si:et:sw=4:sts=4:ts=4:syntax=python
+
+from cerbero.packages import package
+from cerbero.enums import License
+
+class GStreamer:
+
+    url = "http://gstreamer.freedesktop.org"
+    version = '1.9.2'
+    codename = 'Congo'
+    vendor = 'GStreamer Project'
+    licenses = [License.LGPL]
+    org = 'org.freedesktop.gstreamer'
