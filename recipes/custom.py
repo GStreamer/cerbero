66ca0b4a059811df280ae849137e71bcad313e60
Sebastian Dröge <sebastian@centricular.com>
2016-11-01 21:56:53 +0200

    Update versions to 1.10.0

--- recipes/custom.py
+++ recipes/custom.py
@@ -13,8 +13,8 @@ from cerbero.utils import shell, to_unixpath
 class GStreamerBase:
 
     licenses = [License.LGPLv2Plus]
-    version = '1.9'
-    commit = 'origin/master'
+    version = '1.10'
+    commit = '1.10.0'
 
 class GStreamer(GStreamerBase, recipe.Recipe):
 
