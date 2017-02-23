a4eb81dcf3dcda659a82b6c4027a9fa8c5eed9fc
Sebastian Dröge <sebastian@centricular.com>
2017-02-23 16:28:48 +0200

    gstreamer: Update to 1.10.4

--- packages/custom.py
+++ packages/custom.py
@@ -6,7 +6,7 @@ from cerbero.enums import License
 class GStreamer:
 
     url = "http://gstreamer.freedesktop.org"
-    version = '1.10.3'
+    version = '1.10.4'
     vendor = 'GStreamer Project'
     licenses = [License.LGPL]
     org = 'org.freedesktop.gstreamer'
