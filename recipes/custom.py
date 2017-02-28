263deefaa12dfc927e937adc9082ff90d6c935fc
Sebastian Dröge <sebastian@centricular.com>
2017-02-28 11:00:54 +0200

    gstreamer: Update to master / 1.11.2.1 again

--- recipes/custom.py
+++ recipes/custom.py
@@ -13,8 +13,8 @@ from cerbero.utils import shell, to_unixpath
 class GStreamerBase:
 
     licenses = [License.LGPLv2Plus]
-    version = '1.11.2'
-    commit = '1.11.2'
+    version = '1.11.2.1'
+    commit = 'origin/master'
 
 class GStreamer(GStreamerBase, recipe.Recipe):
 
