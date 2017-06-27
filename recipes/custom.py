c430854e4ecbb01a125f95fa468e7fe1d595fda1
Jan Schmidt <jan@centricular.com>
2017-06-27 23:41:55 +1000

    Update to track 1.12 branch tip

--- recipes/custom.py
+++ recipes/custom.py
@@ -13,8 +13,8 @@ from cerbero.utils import shell, to_unixpath
 class GStreamerBase:
 
     licenses = [License.LGPLv2Plus]
-    version = '1.12.1'
-    commit = '1.12.1'
+    version = '1.12.1.1
+    commit = 'origin/1.12'
 
 class GStreamer(GStreamerBase, recipe.Recipe):
 
