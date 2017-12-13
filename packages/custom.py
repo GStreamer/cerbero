3e8b102ba34036ae3e66b163a8e50aa83809381f
Jan Schmidt <jan@centricular.com>
2017-12-14 01:52:30 +1100

    Update to track 1.12 branch tip

--- packages/custom.py
+++ packages/custom.py
@@ -6,7 +6,7 @@ from cerbero.enums import License
 class GStreamer:
 
     url = "http://gstreamer.freedesktop.org"
-    version = '1.12.4'
+    version = '1.12.4.1'
     vendor = 'GStreamer Project'
     licenses = [License.LGPL]
     org = 'org.freedesktop.gstreamer'
--- recipes/custom.py
+++ recipes/custom.py
@@ -13,8 +13,8 @@ from cerbero.utils import shell, to_unixpath
 class GStreamerBase:
 
     licenses = [License.LGPLv2Plus]
-    version = '1.12.4'
-    commit = '1.12.4'
+    version = '1.12.4.1'
+    commit = 'origin/1.12'
 
 class GStreamer(GStreamerBase, recipe.Recipe):
 
