4d9e91db084d5480f5b63ba1be8205e4d2880d71
Olivier Crête <olivier.crete@collabora.com>
2018-01-08 20:19:49 -0500

    config: Do target_distro_version_gte() comparison in right direction

--- cerbero/config.py
+++ cerbero/config.py
@@ -398,7 +398,7 @@ class Config (object):
 
     def target_distro_version_gte(self, distro_version):
         assert distro_version.startswith(self.target_distro + "_")
-        return distro_version >= self.target_distro_version
+        return self.target_distro_version >= distro_version
 
     def _parse(self, filename, reset=True):
         config = {'os': os, '__file__': filename}
