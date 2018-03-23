GStreamer/cerbero

commit a0a134fdf6aadc7d527600b1edc4563b947f07eb

Go to the right directory on failed Universal builds
Instead of picking a random architecture, instead save the failed on in
the exception and use it to go to the right directory.

https://bugzilla.gnome.org/show_bug.cgi?id=772208

cerbero/errors.py
@@ -39,7 +39,8 @@ class UsageError(CerberoException):
 
 class FatalError(CerberoException):
     header = 'Fatal Error: '
-
+    def __init__(self, arch=''):
+        self.arch = arch
 
 class CommandError(CerberoException):
     header = 'Command Error: '
@@ -49,9 +50,10 @@ class BuildStepError(CerberoException):
     recipe = ''
     step = ''
 
-    def __init__(self, recipe, step, trace=''):
+    def __init__(self, recipe, step, trace='', arch=''):
         self.recipe = recipe
         self.step = step
+        self.arch = arch
         CerberoException.__init__(self, _("Recipe '%s' failed at the build "
             "step '%s'\n%s") % (recipe, step, trace))
 

cerbero/build/oven.py
@@ -103,10 +103,10 @@ class Oven (object):
                 action = shell.prompt_multiple(msg, RecoveryActions())
                 if action == RecoveryActions.SHELL:
                     shell.enter_build_environment(self.config.target_platform,
-                            self.config.target_arch, recipe.build_dir)
+                            be.arch, recipe.get_for_arch (be.arch, 'build_dir'))
                     break
                 elif action == RecoveryActions.RETRY_ALL:
-                    shutil.rmtree(recipe.build_dir)
+                    shutil.rmtree(recipe.get_for_arch (be.arch, 'build_dir'))
                     self.cookbook.reset_recipe_status(recipe.name)
                     self._cook_recipe(recipe, i, len(ordered_recipes))
                 elif action == RecoveryActions.RETRY_STEP:
@@ -144,9 +144,9 @@ class Oven (object):
                 # update status successfully
                 self.cookbook.update_step_status(recipe.name, step)
                 shell.close_logfile_output()
-            except FatalError:
+            except FatalError, e:
                 shell.close_logfile_output(dump=True)
-                self._handle_build_step_error(recipe, step)
+                self._handle_build_step_error(recipe, step, e.arch)
             except Exception:
                 shell.close_logfile_output(dump=True)
                 raise BuildStepError(recipe, step, traceback.format_exc())
@@ -156,13 +156,13 @@ class Oven (object):
             self._print_missing_files(recipe, tmp)
             tmp.close()
 
-    def _handle_build_step_error(self, recipe, step):
+    def _handle_build_step_error(self, recipe, step , arch):
         if step in [BuildSteps.FETCH, BuildSteps.EXTRACT]:
             # if any of the source steps failed, wipe the directory and reset
             # the recipe status to start from scratch next time
             shutil.rmtree(recipe.build_dir)
             self.cookbook.reset_recipe_status(recipe.name)
-        raise BuildStepError(recipe, step)
+        raise BuildStepError(recipe, step, arch=arch)
 
     def _print_missing_files(self, recipe, tmp):
         recipe_files = set(recipe.files_list())

cerbero/build/recipe.py
@@ -260,6 +260,9 @@ class Recipe(FilesProvider):
     def _remove_steps(self, steps):
         self._steps = [x for x in self._steps if x not in steps]
 
+    def get_for_arch (self, arch, name):
+        return getattr (self, name)
+
 
 class MetaUniversalRecipe(type):
     '''
@@ -327,11 +330,21 @@ class UniversalRecipe(object):
             for o in self._recipes.values():
                 setattr(o, name, value)
 
+    def get_for_arch (self, arch, name):
+        if arch:
+            return getattr (self._recipes[arch], name)
+        else:
+            return getattr (self, name)
+
     def _do_step(self, step):
         if step in BuildSteps.FETCH:
             # No, really, let's not download a million times...
             stepfunc = getattr(self._recipes.values()[0], step)
-            stepfunc()
+            try:
+                stepfunc()
+            except FatalError, e:
+                e.arch = arch
+                raise e
             return
 
         for arch, recipe in self._recipes.iteritems():
@@ -340,7 +353,11 @@ class UniversalRecipe(object):
             stepfunc = getattr(recipe, step)
 
             # Call the step function
-            stepfunc()
+            try:
+                stepfunc()
+            except FatalError, e:
+                e.arch = arch
+                raise e
 
 
 class UniversalFlatRecipe(UniversalRecipe):

commit a6aaf45348ca1e8a52d261fff6053e0bc5f4902e

cerbero: Fix FatalError message printing
This was inadvertently broken in a0a134fdf

cerbero/errors.py
@@ -39,8 +39,9 @@ class UsageError(CerberoException):
 
 class FatalError(CerberoException):
     header = 'Fatal Error: '
-    def __init__(self, arch=''):
+    def __init__(self, msg='', arch=''):
         self.arch = arch
+        CerberoException.__init__(self, msg)
 
 class CommandError(CerberoException):
     header = 'Command Error: '