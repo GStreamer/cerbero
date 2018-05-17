e8612973499329cc48185e9830c151e501de9934
Tim-Philipp Müller <tim@centricular.com>
2018-05-17 14:15:31 +0100

    Build 1.14.1 release

--- recipes/custom.py
+++ recipes/custom.py
@@ -12,8 +12,8 @@ from cerbero.utils import shell, to_unixpath
 
 class GStreamer(recipe.Recipe):
     licenses = [License.LGPLv2Plus]
-    version = '1.14.0.1'
-    commit = 'origin/1.14'
+    version = '1.14.1'
+    commit = '1.14.1'
 
 def list_gstreamer_1_0_plugins_by_category(config):
         cookbook = CookBook(config)
