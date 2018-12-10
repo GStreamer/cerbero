ca08bc3146be42e01db594bd3b53ef054545b685
Matthew Waters <matthew@centricular.com>
2018-12-10 16:50:38 +1100

    android: Add validate and ges static libraries to package
    As was in the previous 1.14 release of the android package

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -248,6 +248,8 @@ class Recipe(FilesProvider, metaclass=MetaRecipe):
             'gstphotography-1.0' : None,
             'gsturidownloader-1.0' : None,
             'gstrtspserver-1.0' : 'gstreamer-rtsp-server-1.0',
+            'gstvalidate-1.0' : 'gst-validate-1.0',
+            'ges-1.0' : 'gst-editing-services-1.0',
         }
         generated_libs = []
 
