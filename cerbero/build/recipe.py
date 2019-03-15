603e473e062ac8497793ce4110596fd3e8923bb6
Nirbheek Chauhan <nirbheek@centricular.com>
2019-03-15 18:03:56 +0530

    cerbero/windows: Don't generate .lib files for runtime deps
    Nothing should be explicitly linking to this since these are implicit
    deps of DLLs built with the MinGW GCC toolchain.

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -594,6 +594,9 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
         '''
         Generates library files (.lib or .dll.a) for the DLLs provided by this recipe
         '''
+        # Don't need .lib files for runtime-only deps
+        if self.runtime_dep:
+            return
         if output_dir is None:
             output_dir = os.path.join(self.config.prefix,
                                       'lib' + self.config.lib_suffix)
