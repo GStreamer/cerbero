c2b6569ea1b1f55c219aa16f81d302c65b349f45
Nirbheek Chauhan <nirbheek@centricular.com>
2019-04-13 23:26:58 +0530

    gst recipes: Also move back from tarballs to git
    I accidentally pushed the previous incomplete commit straight to master.

--- recipes/custom.py
+++ recipes/custom.py
@@ -13,7 +13,7 @@ from cerbero.utils import shell, to_unixpath
 class GStreamer(recipe.Recipe):
     licenses = [License.LGPLv2Plus]
     version = '1.15.90.1'
-    tagged_for_release = True
+    tagged_for_release = False
     # We want to force the source type to be git when using a manifest else
     # we won't actually test the commit(s) specified in the manifest
     using_manifest_force_git = False
