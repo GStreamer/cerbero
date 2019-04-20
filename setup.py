a3e7da4cedafbad86e5e6b8c585ad3b5155229ae
Nirbheek Chauhan <nirbheek@centricular.com>
2019-04-20 21:55:48 +0530

    Move back to git for gstreamer recipes
    Pushed without a merge request, CI in stable branches is broken:
    https://gitlab.freedesktop.org/gstreamer/gst-ci/issues/11

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class extended_sdist(setuptools_sdist.sdist):
 
 setup(
     name = "cerbero",
-    version = '1.16.0',
+    version = '1.16.0.1',
     author = "Andoni Morales",
     author_email = "amorales@fluendo.com",
     description = ("Multi platform build system for Open Source projects"),
