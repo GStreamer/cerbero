79084c43e00a670eae07fb729682059cae35c4a3
Matthew Waters <matthew@centricular.com>
2019-09-27 16:40:50 +1000

    packages ios: async static library split/joins

--- cerbero/ide/xcode/fwlib.py
+++ cerbero/ide/xcode/fwlib.py
@@ -18,14 +18,17 @@
 # Boston, MA 02111-1307, USA.
 
 import os
+import sys
 import tempfile
 import shutil
 import subprocess
+import asyncio
+import collections
 from collections import defaultdict
 
 from cerbero.config import Architecture
 from cerbero.ide.pkgconfig import PkgConfig
-from cerbero.utils import shell
+from cerbero.utils import shell, run_tasks, run_until_complete
 from cerbero.utils import messages as m
 
 
@@ -86,11 +89,32 @@ class DynamicFrameworkLibrary(FrameworkLibrary):
     def _get_lib_file_name(self, lib):
         return 'lib%s.dylib' % lib
 
+class BuildStatusPrinter:
+    def __init__(self, archs, interactive):
+        self.archs = archs
+        self.interactive = interactive
+        self.arch_total = collections.defaultdict(lambda : 0)
+        self.arch_count = collections.defaultdict(lambda : 0)
+
+    def inc_arch(self, arch):
+        self.arch_count[arch] += 1
+        self.output_status_line()
+
+    def output_status_line(self):
+        if self.interactive:
+            m.output_status(self._generate_status_line())
+
+    def _generate_status_line(self):
+        s = "["
+        s += ", ".join([str(arch) + ": (" + str(self.arch_count[arch]) + "/" + str(self.arch_total[arch]) + ")" for arch in self.archs])
+        s += "]"
+        return s
+
 class StaticFrameworkLibrary(FrameworkLibrary):
     def _get_lib_file_name(self, lib):
         return 'lib%s.a' % lib
 
-    def _split_static_lib(self, lib, thin_arch=None):
+    async def _split_static_lib(self, lib, thin_arch=None):
         '''Splits the static lib @lib into its object files
 
            Splits the static lib @lib into its object files and returns
@@ -107,16 +131,21 @@ class StaticFrameworkLibrary(FrameworkLibrary):
         if thin_arch: #should be a fat file, split only to the arch we want
             newname = '%s_%s' % (thin_arch, os.path.basename(lib))
             cmd = ['lipo', tmplib, '-thin', thin_arch, '-output', newname]
-            try:
-                subprocess.check_output(cmd, cwd=lib_tmpdir, stderr=subprocess.STDOUT,
-                                        universal_newlines=True, env=self.env)
-            except subprocess.CalledProcessError as e:
-                if 'does not contain the specified architecture' in e.output:
+            proc = await asyncio.create_subprocess_exec(*cmd, cwd=lib_tmpdir,
+                stderr=subprocess.PIPE, env=self.env)
+
+            (unused_out, output) = await proc.communicate()
+
+            if sys.stderr.encoding:
+                output = output.decode(sys.stderr.encoding, errors='replace')
+
+            if proc.returncode != 0:
+                if 'does not contain the specified architecture' in output:
                     return None
-                raise
+                raise FatalError('Running {!r}, returncode {}:\n{}'.format(cmd, proc.returncode, output))
             tmplib = os.path.join (lib_tmpdir, newname)
 
-        shell.call('ar -x %s' % tmplib, lib_tmpdir, env=self.env)
+        await shell.async_call(['ar', '-x', tmplib], lib_tmpdir, env=self.env)
 
         # object files with the same name in an archive are overwritten
         # when they are extracted. osx's ar does not support the N count
@@ -126,7 +155,7 @@ class StaticFrameworkLibrary(FrameworkLibrary):
         # ar t libavcodec.a -> mlpdsp.o mlpdsp.o (2 objects with the same name)
         # ar d libavcodec.a mlpdsp.o (we remove the first one)
         # ar t libavcodec.a -> mlpdsp.o (we only the second one now)
-        files = shell.check_call('ar -t %s' % tmplib, lib_tmpdir, env=self.env).split('\n')
+        files = (await shell.async_call_output(['ar', '-t', tmplib], lib_tmpdir, env=self.env)).split('\n')
         # FIXME: We should use collections.Count but it's only available in
         # python 2.7+
         dups = defaultdict(int)
@@ -139,32 +168,31 @@ class StaticFrameworkLibrary(FrameworkLibrary):
                 path = os.path.join(lib_tmpdir, f)
                 new_path = os.path.join(lib_tmpdir, 'dup%d_' % x + f)
                 # The duplicated overwrote the first one, so extract it again
-                shell.call('ar -x %s %s' % (tmplib, f), lib_tmpdir, env=self.env)
+                await shell.async_call(['ar', '-x', tmplib, f], lib_tmpdir, env=self.env)
                 shutil.move (path, new_path)
-                shell.call('ar -d %s %s' % (tmplib, f), lib_tmpdir, env=self.env)
+                await shell.async_call(['ar', '-d',tmplib, f], lib_tmpdir, env=self.env)
 
         return lib_tmpdir
 
-    def _check_duplicated_symbols(self, files, tmpdir):
-        for f in files:
-            syms = defaultdict(list)
-            symbols = shell.check_call('nm -UA %s' % f, tmpdir, env=self.env).split('\n')
-            # nm output is: test.o: 00000000 T _gzwrite
-            # (filename, address, symbol type, symbols_name)
-            for s in symbols:
-                s = s.split(' ')
-                if len(s) == 4 and s[2] == 'T':
-                    syms[s[3]].append(s)
-            dups = {}
-            for k,v in syms.items():
-                if len(v) > 1:
-                    dups[k] = v
-            if dups:
-                m.warning ("The static library contains duplicated symbols")
-            for k, v in dups.items():
-                m.message (k)  # symbol name
-                for l in v:
-                    m.message ("     %s" % l[0])  # file
+    async def _check_duplicated_symbols(self, file, tmpdir):
+        syms = defaultdict(list)
+        symbols = (await shell.async_call_output(['nm', '-UA', file], tmpdir, env=self.env)).split('\n')
+        # nm output is: test.o: 00000000 T _gzwrite
+        # (filename, address, symbol type, symbols_name)
+        for s in symbols:
+            s = s.split(' ')
+            if len(s) == 4 and s[2] == 'T':
+                syms[s[3]].append(s)
+        dups = {}
+        for k,v in syms.items():
+            if len(v) > 1:
+                dups[k] = v
+        if dups:
+            m.warning ("The static library contains duplicated symbols")
+        for k, v in dups.items():
+            m.message (k)  # symbol name
+            for l in v:
+                m.message ("     %s" % l[0])  # file
 
     def _create_framework_library(self, libraries):
         tmpdir = tempfile.mkdtemp()
@@ -178,30 +206,60 @@ class StaticFrameworkLibrary(FrameworkLibrary):
 
         archs = [a if a != Architecture.X86 else 'i386' for a in archs]
 
+        split_queue = asyncio.Queue()
+        join_queues = collections.defaultdict(asyncio.Queue)
         for thin_arch in archs:
-            object_files_md5 = []
             shell.call ('mkdir -p %s' % thin_arch, tmpdir, env=self.env)
-            tmpdir_thinarch = os.path.join(tmpdir, thin_arch)
 
-            for lib in libraries:
+        status = BuildStatusPrinter(archs, m.console_is_interactive())
+        for lib in libraries:
+            for thin_arch in archs:
+                split_queue.put_nowait((lib, thin_arch))
+                status.arch_total[thin_arch] += 1
+
+        async def split_library_worker():
+            while True:
+                lib, thin_arch = await split_queue.get()
+
+                tmpdir_thinarch = os.path.join(tmpdir, thin_arch)
                 libprefix = os.path.split(lib)[-1].replace('.', '_')
 
                 if len(archs) > 1: #should be a fat file, split only to the arch we want
                     libprefix += '_%s_' % thin_arch
-                    lib_tmpdir = self._split_static_lib(lib, thin_arch)
+                    lib_tmpdir = await self._split_static_lib(lib, thin_arch)
                 else:
-                    lib_tmpdir = self._split_static_lib(lib)
+                    lib_tmpdir = await self._split_static_lib(lib)
 
                 if lib_tmpdir is None:
                     # arch is not supported in the static lib, skip it
+                    status.inc_arch (thin_arch)
+                    split_queue.task_done()
                     continue
 
                 obj_files = shell.ls_files(['*.o'], lib_tmpdir)
-                target_objs = []
+                obj_dict = {}
                 for obj_f in obj_files:
                     obj_path = os.path.join(lib_tmpdir, obj_f)
-                    md5 = shell.check_call('md5 -q %s' % obj_path, env=self.env).split('\n')[0]
+                    md5 = (await shell.async_call_output(['md5', '-q', obj_path], env=self.env)).split('\n')[0]
                     md5 = '%s-%s' % (md5, os.path.getsize(obj_path))
+                    obj_dict[obj_f] = md5
+
+                join_queues[thin_arch].put_nowait((lib, lib_tmpdir, obj_dict))
+                split_queue.task_done()
+
+        async def join_library_worker(q, thin_arch):
+            object_files_md5 = []
+            while True:
+                lib, lib_tmpdir, obj_dict = await q.get()
+
+                status.inc_arch (thin_arch)
+
+                tmpdir_thinarch = os.path.join(tmpdir, thin_arch)
+                libprefix = os.path.split(lib)[-1].replace('.', '_')
+
+                target_objs = []
+                for obj_f, md5 in obj_dict.items():
+                    obj_path = os.path.join(lib_tmpdir, obj_f)
                     if md5 not in object_files_md5:
                         target_name = '%s-%s' % (libprefix, obj_f)
                         try:
@@ -209,12 +267,12 @@ class StaticFrameworkLibrary(FrameworkLibrary):
                             os.link(obj_path, tmpdir_thinarch + '/' + target_name)
                         except:
                             # Fall back to cp if hard link doesn't work for any reason
-                            shell.call('cp %s %s' % (obj_path, target_name), tmpdir_thinarch, env=self.env)
+                            await shell.async_call(['cp', obj_path, target_name], tmpdir_thinarch, env=self.env)
 
                         # If we have a duplicate object, commit any collected ones
                         if target_name in target_objs:
                             m.warning ("Committing %d objects due to dup %s" % (len (target_objs), target_name))
-                            shell.call('ar -cqS %s %s' % (libname, " ".join (target_objs)), tmpdir_thinarch, env=self.env)
+                            await shell.async_call(['ar', '-cqS', libname] + target_objs, tmpdir_thinarch, env=self.env)
                             target_objs = []
 
                         target_objs.append (target_name)
@@ -222,15 +280,32 @@ class StaticFrameworkLibrary(FrameworkLibrary):
 
                 # Put all the collected target_objs in the archive. cmdline limit is 262k args on OSX.
                 if len(target_objs):
-                    shell.call('ar -cqS %s %s' % (libname, " ".join (target_objs)), tmpdir_thinarch, env=self.env)
+                    await shell.async_call(['ar', '-cqS', libname] + target_objs, tmpdir_thinarch, env=self.env)
                 shutil.rmtree(lib_tmpdir)
-            shell.call('ar -s %s' % (libname), tmpdir_thinarch, env=self.env)
+                q.task_done()
 
-        files = [os.path.join(tmpdir, arch, libname) for arch in archs]
-        self._check_duplicated_symbols(files, tmpdir)
+        async def post_join_worker(thin_arch):
+            tmpdir_thinarch = os.path.join(tmpdir, thin_arch)
+            await shell.async_call(['ar', '-s', libname], tmpdir_thinarch, env=self.env)
+            lib = os.path.join(tmpdir, thin_arch, libname)
+            await self._check_duplicated_symbols(lib, tmpdir)
+
+        async def split_join_task():
+            tasks = [asyncio.create_task(join_library_worker(join_queues[arch], arch)) for arch in archs]
+            [tasks.append(asyncio.create_task(split_library_worker())) for i in range(len(archs))]
+            async def split_join_queues_done():
+                await split_queue.join()
+                [await join_queues[arch].join() for arch in archs]
+            await run_tasks(tasks, split_join_queues_done())
+
+            tasks = [asyncio.create_task(post_join_worker(thin_arch)) for thin_arch in archs]
+            await run_tasks(tasks)
+        run_until_complete(split_join_task())
+
         if len(archs) > 1:
             #merge the final libs into a fat file again
-            shell.call('lipo %s -create -output %s' % (' '.join(files), self.install_name), tmpdir, env=self.env)
+            files = [os.path.join(tmpdir, arch, libname) for arch in archs]
+            shell.new_call(['lipo'] + files + ['-create' ,'-output', self.install_name], tmpdir, env=self.env)
         else:
-            shell.call('cp %s %s' % (os.path.join(tmpdir, self.arch, libname), self.install_name), tmpdir, env=self.env)
+            shell.new_call(['cp', os.path.join(tmpdir, self.arch, libname), self.install_name], tmpdir, env=self.env)
         shutil.rmtree(tmpdir)
