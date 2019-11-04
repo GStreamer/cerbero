314708156660301c3be149c9e953effe3fd89602
Ruben Gonzalez <rgonzalez@fluendo.com>
2019-11-04 16:59:23 +0100

    Only the dev pkg should contain symlinks for shared libraries w/o version number
    Currently the main packages generetad with cerbero contain symlinks for
    shared libraries without version number. For instance, executing:
    
    ```
    ./cerbero-uninstalled package base-system-1.0
    ```
    
    The `base-system-1.0_1.16.1-1_amd64.deb` contains symlinks like
    `libturbojpeg.so`, `libxml2.so` or `libz.so`.
    
    These symlinks are also in the development package. And there is an error when the development
    package is installed because the system tries to overwrite a file, which was installed by
    the main package.
    
    From debian site:
    https://www.debian.org/doc/manuals/maint-guide/advanced.en.html
    
    > Please note that the development package should contain a symlink for the
    > associated shared library without a version number.
    > E.g.: /usr/lib/x86_64-linux-gnu/libfoo.so -> libfoo.so.1
    
    From the fedora site:
    https://fedoraproject.org/wiki/Archive:BuildingPackagesGuide?rd=Docs/Drafts/BuildingPackagesGuide
    
    > All headers, static libraries, libtool archives, *.so files, autotools,
    > and pkgconfig files go in the -devel subpackage.
    
    With a git bisect, looks like ba26e9c0 is the first bad commit.

--- cerbero/build/filesprovider.py
+++ cerbero/build/filesprovider.py
@@ -124,6 +124,8 @@ class FilesProvider(object):
     # major, minor, micro. We don't use {m,n} here because we want to capture
     # all the matches.
     _SO_REGEX = r'^lib{}\.so(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?$'
+    # Like _SO_REGEX but only libs with version number.
+    _LINUX_SO_REGEX = r'^lib{}\.so(\.[0-9]+)(\.[0-9]+)?(\.[0-9]+)?$'
     _DYLIB_REGEX = r'^lib{}(\.[0-9]+)?(\.[0-9]+)?(\.[0-9]+)?\.dylib$'
 
     # Extension Glob Legend:
@@ -137,7 +139,7 @@ class FilesProvider(object):
     EXTENSIONS = {
         Platform.WINDOWS: {'bext': '.exe', 'sregex': _DLL_REGEX, 'sdir': 'bin',
             'mext': '.dll', 'smext': '.a', 'pext': '.pyd', 'srext': '.dll'},
-        Platform.LINUX: {'bext': '', 'sregex': _SO_REGEX, 'sdir': 'lib',
+        Platform.LINUX: {'bext': '', 'sregex': _LINUX_SO_REGEX, 'sdir': 'lib',
             'mext': '.so', 'smext': '.a', 'pext': '.so', 'srext': '.so'},
         Platform.ANDROID: {'bext': '', 'sregex': _SO_REGEX, 'sdir': 'lib',
             'mext': '.so', 'smext': '.a', 'pext': '.so', 'srext': '.so'},
