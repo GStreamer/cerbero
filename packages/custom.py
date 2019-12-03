4622c81cfd5a3b50ab6871cc8bf0981b4f9fffd6
Tim-Philipp Müller <tim@centricular.com>
2019-12-03 23:53:58 +0000

    Build 1.16.2 release

--- packages/custom.py
+++ packages/custom.py
@@ -6,7 +6,7 @@ from cerbero.enums import License
 class GStreamer:
 
     url = "http://gstreamer.freedesktop.org"
-    version = '1.16.1'
+    version = '1.16.2'
     vendor = 'GStreamer Project'
     licenses = [License.LGPLv2Plus]
     org = 'org.freedesktop.gstreamer'
--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class extended_sdist(setuptools_sdist.sdist):
 
 setup(
     name = "cerbero",
-    version = '1.16.1',
+    version = '1.16.2',
     author = "Andoni Morales",
     author_email = "amorales@fluendo.com",
     description = ("Multi platform build system for Open Source projects"),
