59ab270d3a8309aa370bafc85f011840da96c47f
Nirbheek Chauhan <nirbheek@centricular.com>
2020-02-01 03:27:35 +0530

    cerbero: Disable debian packaging for now
    People keep tripping over this and opening issues. Point people to the
    existing issue in a warning and generate tarballs instead.

--- cerbero/packages/packager.py
+++ cerbero/packages/packager.py
@@ -46,6 +46,13 @@ class Packager (object):
                 "version %s, using generic packager for distro %s" % (v, d)))
             v = None
 
+        if d == Distro.DEBIAN:
+            m.warning('Creation of Debian packages is currently broken, please see '
+                      'https://gitlab.freedesktop.org/gstreamer/cerbero/issues/56\n'
+                      'Creating tarballs instead...')
+            d = Distro.NONE
+            v = None
+
         if d == Distro.WINDOWS and config.cross_compiling():
             try:
                 get_wix_prefix()
