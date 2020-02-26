06192d1d6daa981ceba3d018a4aa323c676bf946
Loïc Le Page <llepage@fluendo.com>
2020-02-26 16:18:11 +0100

    Use asynchronous calls in Strip tool
    And get rid of following error:
    Exception ignored when trying to write to the signal wakeup fd:
    BlockingIOError: [Errno 11] Resource temporarily unavailable

--- cerbero/tools/strip.py
+++ cerbero/tools/strip.py
@@ -18,33 +18,55 @@
 # Boston, MA 02111-1307, USA.
 
 import os
+import asyncio
+import shlex
 from cerbero.config import Platform
-from cerbero.utils import shell
+from cerbero.utils import shell, run_until_complete, messages as m
+
 
 class Strip(object):
     '''Wrapper for the strip tool'''
-
-    STRIP_CMD = '$STRIP'
 
     def __init__(self, config, excludes=None, keep_symbols=None):
         self.config = config
         self.excludes = excludes or []
         self.keep_symbols = keep_symbols or []
+        self.strip_cmd = []
+        if 'STRIP' in config.env:
+            self.strip_cmd = shlex.split(config.env['STRIP'])
 
-    def strip_file(self, path):
+    async def _async_strip_file(self, path):
+        if not self.strip_cmd:
+            m.warning('Strip command is not defined')
+            return
+
         for f in self.excludes:
             if f in path:
                 return
+
+        if self.config.target_platform == Platform.DARWIN:
+            cmd = self.strip_cmd + ['-x', path]
+        else:
+            cmd = self.strip_cmd[:]
+            for symbol in self.keep_symbols:
+                cmd += ['-K', symbol]
+            cmd += ['--strip-unneeded', path]
+
         try:
-            if self.config.target_platform == Platform.DARWIN:
-                shell.call("%s -x %s" % (self.STRIP_CMD, path))
-            else:
-                shell.call("%s %s --strip-unneeded %s" % (self.STRIP_CMD,
-                    ' '.join(['-K %s' % x for x in self.keep_symbols]), path))
-        except:
-            pass
+            await shell.async_call(cmd)
+        except Exception as e:
+            m.warning(e)
+
+    def strip_file(self, path):
+        run_until_complete(self._async_strip_file(path))
 
     def strip_dir(self, dir_path):
+        if not self.strip_cmd:
+            m.warning('Strip command is not defined')
+            return
+
+        tasks = []
         for dirpath, dirnames, filenames in os.walk(dir_path):
             for f in filenames:
-                self.strip_file(os.path.join(dirpath, f))
+                tasks.append(self._async_strip_file(os.path.join(dirpath, f)))
+        run_until_complete(tasks)
