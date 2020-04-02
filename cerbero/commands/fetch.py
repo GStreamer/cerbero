d211542a900bb9f44254876ada7277e3ba78fdf3
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2020-04-02 17:42:05 -0400

    cache: Re-implememt cache on top of our external storage
    The goal is to reduce egress on FDO server. In this patch, the cache is split
    from fetch.py into it's own set of commands in cache.py. It now implements
    fetch-cache, gen-cache and upload-chache. upload-cache can only work if the
    SSH private key needed to upload has been set into CERBERO_PRIVATE_SSH_KEY
    environment or is available in your .ssh folder. The key will be made availabe
    through configuration in gstreamer/cerbero CI configuration and only
    available to protected branches on the gstreamer namespace.
    
    Backported-by: Nirbheek Chauhan <nirbheek@centricular.com>

--- cerbero/commands/fetch.py
+++ cerbero/commands/fetch.py
@@ -16,12 +16,7 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-import os, sys
-import urllib
-import json
 import asyncio
-import tempfile
-import shutil
 
 from cerbero.commands import Command, register_command
 from cerbero.build.cookbook import CookBook
@@ -30,7 +25,6 @@ from cerbero.packages.packagesstore import PackagesStore
 from cerbero.utils import _, N_, ArgparseArgument, remove_list_duplicates, git, shell
 from cerbero.utils import messages as m
 from cerbero.build.source import Tarball
-from cerbero.config import Distro
 
 
 class Fetch(Command):
@@ -134,125 +128,5 @@ class FetchPackage(Fetch):
                           args.deps, args.reset_rdeps, args.full_reset,
                           args.print_only)
 
-class FetchCache(Command):
-    doc = N_('Fetch a cached build from GitLab CI based on cerbero git '
-            'revision.')
-    name = 'fetch-cache'
-
-    base_url = 'https://gitlab.freedesktop.org/%s/cerbero/-/jobs'
-    build_dir = '/builds/%s/cerbero/cerbero-build'
-    log_size = 50
-
-    def __init__(self, args=[]):
-        args.append(ArgparseArgument('--commit', action='store', type=str,
-                    default='HEAD', help=_('the commit to pick artifact from')))
-        args.append(ArgparseArgument('--namespace', action='store', type=str,
-                    default='gstreamer', help=_('GitLab namespace to search from')))
-        args.append(ArgparseArgument('--branch', action='store', type=str,
-                    default='master', help=_('Git branch to search from')))
-        args.append(ArgparseArgument('--job-id', action='store', type=str,
-                    default='master', help=_('Artifact job id, this will skip'
-                        ' commit matching')))
-        args.append(ArgparseArgument('--skip-fetch', action='store_true',
-                    default=False, help=_('Skip fetching cached build, the '
-                        'commit/url log will be updated if --job-id is present')))
-        Command.__init__(self, args)
-
-    def request(self, url, values, token=None):
-        headers = {}
-        if token:
-            headers = {"Private-Token": token}
-
-        data = urllib.parse.urlencode(values)
-        url = "%s?%s" % (url, data)
-
-        m.message("GET %s" % url)
-
-        tmpdir = tempfile.mkdtemp()
-        tmpfile = os.path.join(tmpdir, 'deps.json')
-
-        try:
-            shell.download(url, destination=tmpfile)
-        except urllib.error.URLError as e:
-            raise FatalError(_(e.reason))
-
-        with open(tmpfile, 'r') as f:
-            resp = f.read()
-        shutil.rmtree(tmpdir)
-
-        return json.loads(resp)
-
-    def get_deps(self, config, args):
-        namespace = args.namespace
-        branch = args.branch
-        distro = config.target_distro
-        arch = config.target_arch
-        if distro == Distro.REDHAT:
-            distro = 'fedora'
-        if distro == Distro.OS_X:
-            distro = 'macos'
-
-        base_url = self.base_url % namespace
-        url = "%s/artifacts/%s/raw/cerbero-build/cerbero-deps.log" % (base_url, branch)
-
-        deps = []
-        try:
-            deps = self.request(url, values = {
-                    'job': "cerbero deps %s %s" % (distro, arch)
-                    })
-        except FatalError as e:
-            m.warning("Could not get cache list: %s" % e.msg)
-
-        return deps
-
-    def find_dep(self, deps, sha):
-        for dep in deps:
-            if dep['commit'] == sha:
-                return dep
-
-        m.warning("Did not find cache for commit %s" % sha)
-        return None
-
-    def fetch_dep(self, config, dep, namespace):
-        try:
-            artifacts_path = "%s/cerbero-deps.tar.gz" % config.home_dir
-            shell.download(dep['url'], artifacts_path, check_cert=True, overwrite=True)
-            shell.unpack(artifacts_path, config.home_dir)
-            os.remove(artifacts_path)
-            origin = self.build_dir % namespace
-            m.message("Relocating from %s to %s" % (origin, config.home_dir))
-            # FIXME: Just a quick hack for now
-            shell.call(("grep -lnrIU %(origin)s | xargs "
-                        "sed \"s#%(origin)s#%(dest)s#g\" -i") % {
-                            'origin': origin, 'dest': config.home_dir},
-                        config.home_dir)
-        except FatalError as e:
-            m.warning(("Could not retrieve artifact for commit %s (the artifact "
-                    "may have expired): %s") % (dep['commit'], e.msg))
-
-    def update_log(self, config, args, deps, sha):
-        base_url = self.base_url % args.namespace
-        url = "%s/%s/artifacts/raw/cerbero-deps.tar.gz" % (base_url, args.job_id)
-        deps.insert(0, {'commit': sha, 'url': url})
-        deps = deps[0:self.log_size]
-        with open("%s/cerbero-deps.log" % config.home_dir, 'w') as outfile:
-                json.dump(deps, outfile, indent=1)
-
-    def run(self, config, args):
-        if not config.uninstalled:
-            raise FatalError(_("fetch-cache is only available with "
-                        "cerbero-uninstalled"))
-
-        git_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-        sha = git.get_hash(git_dir, args.commit)
-        deps = self.get_deps(config, args)
-        if not args.skip_fetch:
-            dep = self.find_dep(deps, sha)
-            if dep:
-                self.fetch_dep(config, dep, args.namespace)
-        if args.job_id:
-            self.update_log(config, args, deps, sha)
-
 register_command(FetchRecipes)
 register_command(FetchPackage)
-register_command(FetchCache)
