GStreamer/cerbero

commit e3892148653bd96a1d1f8f370809762dc8903795

data: Move from gnutls to openssl for android/ios

data/ndk-build/gstreamer-1.0.mk
@@ -153,8 +153,6 @@ G_IO_MODULES_LOAD            := $(foreach module, $(G_IO_MODULES), \
 GSTREAMER_ANDROID_LIBS       := $(call pkg-config-get-libs,$(GSTREAMER_DEPS))
 GSTREAMER_ANDROID_LIBS       += $(GSTREAMER_PLUGINS_LIBS) $(G_IO_MODULES_LIBS) -llog -lz
 GSTREAMER_ANDROID_WHOLE_AR   := $(call pkg-config-get-libs-no-deps,$(GSTREAMER_DEPS)) $(GSTREAMER_EXTRA_LIBS)
-# Fix deps for giognutls
-GSTREAMER_ANDROID_LIBS       := $(call fix-deps,-lgiognutls, -lhogweed) $(GSTREAMER_EXTRA_LIBS)
 GSTREAMER_ANDROID_CFLAGS     := $(call pkg-config-get-includes,$(GSTREAMER_DEPS)) -I$(GSTREAMER_ROOT)/include
 
 # In newer NDK, SYSROOT is replaced by SYSROOT_INC and SYSROOT_LINK, which

data/xcode/templates/ios/GStreamer Base.xctemplate/gst_ios_init.h
@@ -17,7 +17,7 @@ G_PASTE(g_io_, G_PASTE(name, _load)) (NULL)
 
 @GST_IOS_PLUGINS_CATEGORIES@
 
-//#define GST_IOS_GIO_MODULE_GNUTLS
+//#define GST_IOS_GIO_MODULE_OPENSSL
 
 void gst_ios_init (void);
 

data/xcode/templates/ios/GStreamer Base.xctemplate/gst_ios_init.m
@@ -4,8 +4,8 @@
 
 @GST_IOS_PLUGINS_DECLARE@
 
-#if defined(GST_IOS_GIO_MODULE_GNUTLS)
-  GST_G_IO_MODULE_DECLARE(gnutls);
+#if defined(GST_IOS_GIO_MODULE_OPENSSL)
+  GST_G_IO_MODULE_DECLARE(openssl);
 #endif
 
 void
@@ -40,8 +40,8 @@ gst_ios_init (void)
   ca_certificates = g_build_filename (resources_dir, "ssl", "certs", "ca-certificates.crt", NULL);
   g_setenv ("CA_CERTIFICATES", ca_certificates, TRUE);
 
-#if defined(GST_IOS_GIO_MODULE_GNUTLS)
-  GST_G_IO_MODULE_LOAD(gnutls);
+#if defined(GST_IOS_GIO_MODULE_OPENSSL)
+  GST_G_IO_MODULE_LOAD(openssl);
 #endif
 
   if (ca_certificates) {

commit 93257a7f17453f8d75100bbfda5716a3f752d3eb

ndk-build: keep linking to GSTREAMER_EXTRA_LIBS explicitly
When removing hogweed linkage for gio workaround, the $GSTREAMER_EXTRA_LIBS
entry got removed from GSTREAMER_ANDROID_LIBS as well. Add it back, so it
works as intended in commit d4d75a04065 and needed linkages like EGL work
again via GSTREAMER_EXTRA_LIBS `-l` entries (instead of having to workaround
via TARGET_LDFLAGS).

Fixes: e3892148653bd96a1d1f8f370809762dc8903795

data/ndk-build/gstreamer-1.0.mk
@@ -151,7 +151,7 @@ G_IO_MODULES_LOAD            := $(foreach module, $(G_IO_MODULES), \
 # Get the full list of libraries
 # link at least to gstreamer-1.0 in case the plugins list is empty
 GSTREAMER_ANDROID_LIBS       := $(call pkg-config-get-libs,$(GSTREAMER_DEPS))
-GSTREAMER_ANDROID_LIBS       += $(GSTREAMER_PLUGINS_LIBS) $(G_IO_MODULES_LIBS) -llog -lz
+GSTREAMER_ANDROID_LIBS       += $(GSTREAMER_PLUGINS_LIBS) $(G_IO_MODULES_LIBS) $(GSTREAMER_EXTRA_LIBS) -llog -lz
 GSTREAMER_ANDROID_WHOLE_AR   := $(call pkg-config-get-libs-no-deps,$(GSTREAMER_DEPS)) $(GSTREAMER_EXTRA_LIBS)
 GSTREAMER_ANDROID_CFLAGS     := $(call pkg-config-get-includes,$(GSTREAMER_DEPS)) -I$(GSTREAMER_ROOT)/include
 