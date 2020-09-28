GStreamer/cerbero

commit 49ab89744de42e6f3dbb13e1090b75371b5a8000

Split bootstrap commands into system/toolchains/build-tools
This allows us to run the --system command in the docker file when
building the image, and --toolchains and --build-tools when running
a CI job.

This will simplify the job scripts, and will also allow us to test
changes to the toolchain code in cerbero without rebuilding the image.

Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/611>

cerbero/bootstrap/bootstrapper.py
@@ -34,14 +34,11 @@ def register_bootstrapper(distro, klass, distro_version=None):
 
 
 class Bootstrapper (object):
-    def __new__(klass, config, build_tools_only, offline, assume_yes,
-            system_only):
+    def __new__(klass, config, system, toolchains, build_tools, offline, assume_yes):
         bs = []
 
-        if not system_only:
+        if build_tools:
             bs.append(BuildTools(config, offline))
-        if build_tools_only:
-            return bs
 
         target_distro = config.target_distro
         distro = config.distro
@@ -55,10 +52,11 @@ class Bootstrapper (object):
         target = (target_distro, target_distro_version)
         build = (distro, distro_version)
 
-        if target == build:
-            blist = [target]
-        else:
-            blist = [target, build]
+        blist = []
+        if system:
+            blist.append(build)
+        if toolchains and build != target:
+            blist.append(target)
 
         for d, v in blist:
             if d not in bootstrappers:

cerbero/commands/bootstrap.py
@@ -17,9 +17,10 @@
 # Boston, MA 02111-1307, USA.
 
 import asyncio
+import argparse
 
 from cerbero.commands import Command, register_command
-from cerbero.utils import N_, _, ArgparseArgument, determine_num_of_cpus, run_until_complete
+from cerbero.utils import N_, _, ArgparseArgument, determine_num_of_cpus, run_until_complete, StoreBool
 from cerbero.utils import messages as m
 from cerbero.bootstrap.bootstrapper import Bootstrapper
 
@@ -33,9 +34,18 @@ class Bootstrap(Command):
     def __init__(self):
         args = [
             ArgparseArgument('--build-tools-only', action='store_true',
-                default=False, help=_('only bootstrap the build tools')),
+                default=False, help=argparse.SUPPRESS),
             ArgparseArgument('--system-only', action='store_true',
-                default=False, help=('only boostrap the system')),
+                default=False, help=argparse.SUPPRESS),
+            ArgparseArgument('--system', action=StoreBool,
+                default=True, nargs='?', choices=('yes', 'no'),
+                help='Setup the system for building, such as by installing system packages'),
+            ArgparseArgument('--toolchains', action=StoreBool,
+                default=True, nargs='?', choices=('yes', 'no'),
+                help='Setup any toolchains needed by the target platform'),
+            ArgparseArgument('--build-tools', action=StoreBool,
+                default=True, nargs='?', choices=('yes', 'no'),
+                help='Compile the build tools needed while building'),
             ArgparseArgument('--offline', action='store_true',
                 default=False, help=_('Use only the source cache, no network')),
             ArgparseArgument('-y', '--assume-yes', action='store_true',
@@ -46,8 +56,16 @@ class Bootstrap(Command):
         Command.__init__(self, args)
 
     def run(self, config, args):
-        bootstrappers = Bootstrapper(config, args.build_tools_only,
-                args.offline, args.assume_yes, args.system_only)
+        if args.build_tools_only:
+            args.toolchains = False
+            args.system = False
+            m.deprecation('--build-tools-only has been replaced by --system/toolchains/build-tools')
+        if args.system_only:
+            args.build_tools = False
+            args.toolchains = False
+            m.deprecation('--system-only has been replaced by --system/toolchains/build-tools')
+        bootstrappers = Bootstrapper(config, args.system, args.toolchains,
+                args.build_tools, args.offline, args.assume_yes)
         tasks = []
         async def bootstrap_fetch_extract(bs):
             await bs.fetch()
@@ -67,14 +85,24 @@ class FetchBootstrap(Command):
     def __init__(self):
         args = [
             ArgparseArgument('--build-tools-only', action='store_true',
-                default=False, help=_('only fetch the build tools')),
+                default=False, help=argparse.SUPPRESS),
+            ArgparseArgument('--toolchains', action=StoreBool,
+                default=True, nargs='?', choices=('yes', 'no'),
+                help='Setup any toolchains needed by the target platform'),
+            ArgparseArgument('--build-tools', action=StoreBool,
+                default=True, nargs='?', choices=('yes', 'no'),
+                help='Compile the build tools needed while building'),
             ArgparseArgument('--jobs', '-j', action='store', nargs='?', type=int,
                     const=NUMBER_OF_JOBS_IF_USED, default=NUMBER_OF_JOBS_IF_UNUSED, help=_('number of async jobs'))]
         Command.__init__(self, args)
 
     def run(self, config, args):
-        bootstrappers = Bootstrapper(config, args.build_tools_only,
-                offline=False, assume_yes=False, system_only=False)
+        if args.build_tools_only:
+            args.toolchains = False
+            args.system = False
+            m.deprecation('--build-tools-only has been replaced by --system/toolchains/build-tools')
+        bootstrappers = Bootstrapper(config, False, args.toolchains,
+                args.build_tools, offline=False, assume_yes=False)
         tasks = []
         for bootstrapper in bootstrappers:
             bootstrapper.fetch_recipes(args.jobs)

cerbero/utils/__init__.py
@@ -20,6 +20,7 @@ import os
 import sys
 import shlex
 import shutil
+import argparse
 try:
     import sysconfig
 except:
@@ -52,6 +53,21 @@ class ArgparseArgument(object):
 
     def add_to_parser(self, parser):
         parser.add_argument(*self.name, **self.args)
+
+
+class StoreBool(argparse.Action):
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        if value == 'yes':
+            bvalue = True
+        elif value == 'no':
+            bvalue = False
+        else:
+            raise AssertionError
+        setattr(namespace, self.dest, bvalue)
 
 
 def user_is_root():

cerbero/utils/messages.py
@@ -110,6 +110,10 @@ def error(msg, logfile=None):
         log(msg, logfile=logfile)
 
 
+def deprecation(msg, logfile=None):
+    error("DEPRECATION: %s" % msg, logfile=logfile)
+
+
 def warning(msg, logfile=None):
     error("WARNING: %s" % msg, logfile=logfile)
 

commit ae8c7b75e252a83f33c7dac52d1ee0d28aa53e71

bootstrap: Reclassify bootstrappers as system or toolchain
Some bootstrappers are responsible for bootstrapping the system
(usually the 'build distro' bootstrappers), and others are responsible
for bootstrapping the toolchain that will be used for building
(usually the 'target distro' bootstrappers).

We need to differentiate this so we can split the Windows bootstrapper
into separate classes for the toolchain (MinGW + other deps, on
Native-Windows and Cross-Windows) and MSYS (only on Native-Windows).

This also fixes downloading and extracting of toolchains in bootstrap
which broke in 49ab89744de42e6f3dbb13e1090b75371b5a8000

Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/616>

cerbero/bootstrap/android.py
@@ -20,7 +20,7 @@ import os
 import shutil
 
 from cerbero.bootstrap import BootstrapperBase
-from cerbero.bootstrap.bootstrapper import register_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_toolchain_bootstrapper
 from cerbero.config import Distro, FatalError
 from cerbero.utils import _, shell
 
@@ -58,4 +58,4 @@ class AndroidBootstrapper (BootstrapperBase):
 
 
 def register_all():
-    register_bootstrapper(Distro.ANDROID, AndroidBootstrapper)
+    register_toolchain_bootstrapper(Distro.ANDROID, AndroidBootstrapper)

cerbero/bootstrap/bootstrapper.py
@@ -24,21 +24,24 @@ from cerbero.utils import messages as m
 from cerbero.bootstrap.build_tools import BuildTools
 
 
-bootstrappers = {}
+toolchain_bootstrappers = {}
+system_bootstrappers = {}
 
 
-def register_bootstrapper(distro, klass, distro_version=None):
-    if not distro in bootstrappers:
-        bootstrappers[distro] = {}
-    bootstrappers[distro][distro_version] = klass
+def register_system_bootstrapper(distro, klass, distro_version=None):
+    if not distro in system_bootstrappers:
+        system_bootstrappers[distro] = {}
+    system_bootstrappers[distro][distro_version] = klass
+
+def register_toolchain_bootstrapper(distro, klass, distro_version=None):
+    if not distro in toolchain_bootstrappers:
+        toolchain_bootstrappers[distro] = {}
+    toolchain_bootstrappers[distro][distro_version] = klass
 
 
 class Bootstrapper (object):
     def __new__(klass, config, system, toolchains, build_tools, offline, assume_yes):
         bs = []
-
-        if build_tools:
-            bs.append(BuildTools(config, offline))
 
         target_distro = config.target_distro
         distro = config.distro
@@ -52,19 +55,30 @@ class Bootstrapper (object):
         target = (target_distro, target_distro_version)
         build = (distro, distro_version)
 
-        blist = []
+        blist = [target]
+        if target != build:
+            blist += [build]
+
+        all_bootstrappers = {}
+        # If enabled, run system bootstrappers first
         if system:
-            blist.append(build)
-        if toolchains and build != target:
-            blist.append(target)
+            all_bootstrappers.update(system_bootstrappers)
+        if toolchains:
+            all_bootstrappers.update(toolchain_bootstrappers)
 
         for d, v in blist:
-            if d not in bootstrappers:
+            if d not in all_bootstrappers:
+                # Skip if we were asked to skip some bootstrapping
+                if not system or not toolchains:
+                    continue
                 raise FatalError(_("No bootstrapper for the distro %s" % d))
-            if v not in bootstrappers[d]:
+            if v not in all_bootstrappers[d]:
                 v = None
+            bs.append(all_bootstrappers[d][v](config, offline, assume_yes))
 
-            bs.insert(0, bootstrappers[d][v](config, offline, assume_yes))
+        # Build the build-tools after all other bootstrappers
+        if build_tools:
+            bs.append(BuildTools(config, offline))
 
         return bs
 

cerbero/bootstrap/ios.py
@@ -24,7 +24,7 @@
 # any later version.
 
 from cerbero.bootstrap import BootstrapperBase
-from cerbero.bootstrap.bootstrapper import register_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_toolchain_bootstrapper
 from cerbero.config import Distro
 
 
@@ -39,4 +39,4 @@ class IOSBootstrapper (BootstrapperBase):
 
 
 def register_all():
-    register_bootstrapper(Distro.IOS, IOSBootstrapper)
+    register_toolchain_bootstrapper(Distro.IOS, IOSBootstrapper)

cerbero/bootstrap/linux.py
@@ -17,7 +17,7 @@
 # Boston, MA 02111-1307, USA.
 
 from cerbero.bootstrap import BootstrapperBase
-from cerbero.bootstrap.bootstrapper import register_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_system_bootstrapper
 from cerbero.enums import Platform, Architecture, Distro, DistroVersion
 from cerbero.errors import ConfigurationError
 from cerbero.utils import shell
@@ -195,9 +195,9 @@ class NoneBootstrapper (BootstrapperBase):
 
 
 def register_all():
-    register_bootstrapper(Distro.DEBIAN, DebianBootstrapper)
-    register_bootstrapper(Distro.REDHAT, RedHatBootstrapper)
-    register_bootstrapper(Distro.SUSE, OpenSuseBootstrapper)
-    register_bootstrapper(Distro.ARCH, ArchBootstrapper, DistroVersion.ARCH_ROLLING)
-    register_bootstrapper(Distro.GENTOO, GentooBootstrapper, DistroVersion.GENTOO_VERSION)
-    register_bootstrapper(Distro.NONE, NoneBootstrapper)
+    register_system_bootstrapper(Distro.DEBIAN, DebianBootstrapper)
+    register_system_bootstrapper(Distro.REDHAT, RedHatBootstrapper)
+    register_system_bootstrapper(Distro.SUSE, OpenSuseBootstrapper)
+    register_system_bootstrapper(Distro.ARCH, ArchBootstrapper, DistroVersion.ARCH_ROLLING)
+    register_system_bootstrapper(Distro.GENTOO, GentooBootstrapper, DistroVersion.GENTOO_VERSION)
+    register_system_bootstrapper(Distro.NONE, NoneBootstrapper)

cerbero/bootstrap/osx.py
@@ -19,7 +19,7 @@
 import os
 
 from cerbero.bootstrap import BootstrapperBase
-from cerbero.bootstrap.bootstrapper import register_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_system_bootstrapper
 from cerbero.config import Distro
 from cerbero.utils import shell
 from cerbero.utils import messages as m
@@ -52,4 +52,4 @@ class OSXBootstrapper (BootstrapperBase):
 
 
 def register_all():
-    register_bootstrapper(Distro.OS_X, OSXBootstrapper)
+    register_system_bootstrapper(Distro.OS_X, OSXBootstrapper)

cerbero/bootstrap/windows.py
@@ -22,7 +22,8 @@ import shutil
 from pathlib import Path
 
 from cerbero.bootstrap import BootstrapperBase
-from cerbero.bootstrap.bootstrapper import register_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_system_bootstrapper
+from cerbero.bootstrap.bootstrapper import register_toolchain_bootstrapper
 from cerbero.config import Architecture, Distro, Platform
 from cerbero.errors import ConfigurationError
 from cerbero.utils import shell, _, fix_winpath, to_unixpath, git
@@ -56,7 +57,48 @@ XZ_CHECKSUM = 'd83b82ca75dfab39a13dda364367b34970c781a9df4d41264db922ac3a8f622d'
 # MSYS packages needed
 MINGWGET_DEPS = ['msys-flex', 'msys-bison', 'msys-perl']
 
-class WindowsBootstrapper(BootstrapperBase):
+class MSYSBootstrapper(BootstrapperBase):
+    '''
+    Bootstrapper for native windows builds on top of MSYS
+    Installs the necessary MSYS packages and fixups
+    '''
+
+    def __init__(self, config, offline, assume_yes):
+        super().__init__(config, offline)
+
+    def start(self, jobs=0):
+        self.install_mingwget_deps() # FIXME: This uses the network
+        self.fix_mingw_unused()
+
+    def install_mingwget_deps(self):
+        for dep in MINGWGET_DEPS:
+            shell.new_call(['mingw-get', 'install', dep])
+
+    def fix_mingw_unused(self):
+        mingw_get_exe = shutil.which('mingw-get')
+        if not mingw_get_exe:
+            m.warning('mingw-get not found, are you not using an MSYS shell?')
+            return
+        msys_mingw_bindir = Path(mingw_get_exe).parent
+        # Fixes checks in configure, where cpp -v is called
+        # to get some include dirs (which doesn't looks like a good idea).
+        # If we only have the host-prefixed cpp, this problem is gone.
+        if (msys_mingw_bindir / 'cpp.exe').is_file():
+            os.replace(msys_mingw_bindir / 'cpp.exe',
+                       msys_mingw_bindir / 'cpp.exe.bck')
+        # MSYS's link.exe (for symlinking) overrides MSVC's link.exe (for
+        # C linking) in new shells, so rename it. No one uses `link` for
+        # symlinks anyway.
+        msys_link_exe = shutil.which('link')
+        if not msys_link_exe:
+            return
+        msys_link_exe = Path(msys_link_exe)
+        msys_link_bindir = msys_link_exe.parent
+        if msys_link_exe.is_file() and 'msys/1.0/bin/link' in msys_link_exe.as_posix():
+            os.replace(msys_link_exe, msys_link_bindir / 'link.exe.bck')
+
+
+class MinGWBootstrapper(BootstrapperBase):
     '''
     Bootstrapper for windows builds.
     Installs the mingw-w64 compiler toolchain and headers for Directx
@@ -111,8 +153,6 @@ class WindowsBootstrapper(BootstrapperBase):
             self.fix_mingw()
             self.fix_openssl_mingw_perl()
             self.fix_bin_deps()
-            self.install_mingwget_deps() # FIXME: This uses the network
-            self.fix_mingw_unused()
             self.install_xz()
 
     def check_dirs(self):
@@ -154,10 +194,6 @@ class WindowsBootstrapper(BootstrapperBase):
             shutil.move(os.path.join(perldir, d), self.perl_prefix)
         os.rmdir(perldir)
 
-    def install_mingwget_deps(self):
-        for dep in MINGWGET_DEPS:
-            shell.new_call(['mingw-get', 'install', dep])
-
     def install_xz(self):
         msys_xz = shutil.which('xz')
         if not msys_xz:
@@ -174,29 +210,7 @@ class WindowsBootstrapper(BootstrapperBase):
             shell.replace(os.path.join(self.prefix, f),
                           {'/opt/perl/bin/perl': '/bin/perl'})
 
-    def fix_mingw_unused(self):
-        mingw_get_exe = shutil.which('mingw-get')
-        if not mingw_get_exe:
-            m.warning('mingw-get not found, are you not using an MSYS shell?')
-            return
-        msys_mingw_bindir = Path(mingw_get_exe).parent
-        # Fixes checks in configure, where cpp -v is called
-        # to get some include dirs (which doesn't looks like a good idea).
-        # If we only have the host-prefixed cpp, this problem is gone.
-        if (msys_mingw_bindir / 'cpp.exe').is_file():
-            os.replace(msys_mingw_bindir / 'cpp.exe',
-                       msys_mingw_bindir / 'cpp.exe.bck')
-        # MSYS's link.exe (for symlinking) overrides MSVC's link.exe (for
-        # C linking) in new shells, so rename it. No one uses `link` for
-        # symlinks anyway.
-        msys_link_exe = shutil.which('link')
-        if not msys_link_exe:
-            return
-        msys_link_exe = Path(msys_link_exe)
-        msys_link_bindir = msys_link_exe.parent
-        if msys_link_exe.is_file() and 'msys/1.0/bin/link' in msys_link_exe.as_posix():
-            os.replace(msys_link_exe, msys_link_bindir / 'link.exe.bck')
-
 
 def register_all():
-    register_bootstrapper(Distro.WINDOWS, WindowsBootstrapper)
+    register_toolchain_bootstrapper(Distro.WINDOWS, MinGWBootstrapper)
+    register_system_bootstrapper(Distro.WINDOWS, MSYSBootstrapper)