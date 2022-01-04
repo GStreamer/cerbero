f7fb40564858bed0ed892599d928b16aaae8da6e
Nirbheek Chauhan <nirbheek@centricular.com>
2022-01-05 01:11:33 +0530

    cerbero: Fix support for Fedora 35
    NAME in /etc/os-release is now Fedora Linux
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/779>

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -249,7 +249,7 @@ Terminating.''', file=sys.stderr)
                 distro_version = "debian_{number:02d}_{name}".format(number=int(d[1]), name=d[2])
             else:
                 raise FatalError("Distribution '%s' not supported" % str(d))
-        elif d[0] in ['RedHat', 'Fedora', 'CentOS', 'Red Hat Enterprise Linux Server', 'CentOS Linux']:
+        elif d[0] in ['RedHat', 'Fedora', 'Fedora Linux', 'CentOS', 'Red Hat Enterprise Linux Server', 'CentOS Linux']:
             distro = Distro.REDHAT
             if d[1] == '16':
                 distro_version = DistroVersion.FEDORA_16
@@ -285,7 +285,7 @@ Terminating.''', file=sys.stderr)
                 distro_version = DistroVersion.FEDORA_31
             elif d[1] == '32':
                 distro_version = DistroVersion.FEDORA_32
-            elif d[0] == 'Fedora':
+            elif d[0].startswith('Fedora'):
                 # str(int()) is for ensuring that the fedora version is
                 # actually a number
                 distro_version = 'fedora_' + str(int(d[1]))
