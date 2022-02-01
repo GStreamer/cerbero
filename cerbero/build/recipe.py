67b2fdfb78bc51db0358b672af8df66762cc3664
Nirbheek Chauhan <nirbheek@centricular.com>
2022-02-01 14:48:07 +0530

    cerbero: Try again to solve relocate_osx_libraries crash in CI
    A fix was previously attempted in 7a53942b4e4cb139871f22f27c3a5fc11dbfdc9a
    but it seems to have been insufficient since the crash is still
    happening from time to time on the CI:
    
    Traceback (most recent call last):
      File "<string>", line 20, in <module>
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/main.py", line 183, in main
        Main(sys.argv[1:])
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/main.py", line 53, in __init__
        self.run_command()
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/main.py", line 152, in run_command
        res = commands.run(command, self.config, self.args)
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/commands/__init__.py", line 78, in run
        return _commands[command].run(config, args)
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/commands/bootstrap.py", line 79, in run
        bootstrapper.start(jobs=args.jobs)
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/bootstrap/build_tools.py", line 131, in start
        oven.start_cooking()
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/build/oven.py", line 131, in start_cooking
        run_until_complete(self._cook_recipes(ordered_recipes))
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/utils/__init__.py", line 647, in run_until_complete
        result = loop.run_until_complete(tasks)
      File "/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/asyncio/base_events.py", line 612, in run_until_complete
        return future.result()
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/build/oven.py", line 415, in _cook_recipes
        raise e
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/build/oven.py", line 412, in _cook_recipes
        await run_tasks(tasks, recipes_done())
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/utils/__init__.py", line 695, in run_tasks
        await shutdown()
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/utils/__init__.py", line 682, in shutdown
        raise e
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/utils/__init__.py", line 689, in run_tasks
        await asyncio.gather(*tasks, return_exceptions=False)
      File "/Users/gst-ci/builds/gstreamer/cerbero/cerbero/build/oven.py", line 309, in cook_recipe_worker
        next_queue = queues[step]
    KeyError: 'relocate_osx_libraries'
    
    https://gitlab.freedesktop.org/gstreamer/cerbero/-/jobs/18307540
    
    I can't reproduce it locally, probably because it requires a high
    degree of parallelism / the right conditions which are only achieved
    on the CI.
    
    It's likely happening because the recipe step scheduler assumes that
    all recipes have the same steps. So instead of removing the step, skip
    the step inside the method when requested.
    
    Also remove some obsoleted code / comments.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/792>

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -272,7 +272,13 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
             config.bash_completions.update(self.bash_completions)
             self.deps.append('bash-completion')
         self.platform_deps = self.platform_deps or {}
+
         self.skip_steps = self.skip_steps or []
+        allowed_skip = {BuildSteps.RELOCATE_OSX_LIBRARIES, BuildSteps.CODE_SIGN}
+        bad_skip = set(self.skip_steps) - allowed_skip
+        if bad_skip:
+            raise FatalError(f"Can only skip steps {allowed_skip}, not {bad_skip}")
+
         self._steps = self._default_steps[:]
         if self.config.target_platform == Platform.WINDOWS:
             self._steps.append(BuildSteps.GEN_LIBFILES)
@@ -281,9 +287,7 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
         if self.config.target_platform == Platform.DARWIN and \
                 self.config.prefix == self.config.build_tools_prefix:
             self._steps.append(BuildSteps.CODE_SIGN)
-        for s in self.skip_steps:
-            if s in self._steps:
-                self._steps.remove(s)
+
         FilesProvider.__init__(self, config)
         try:
             self.stype.__init__(self)
@@ -314,7 +318,7 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
     def prepare(self):
         '''
         Can be overriden by subclasess to modify the recipe in function of
-        the configuration, like modifying steps for a given platform
+        the configuration
         '''
         pass
 
@@ -486,8 +490,9 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
         '''
         Make OSX libraries relocatable
         '''
-        relocator = OSXRelocator(self.config.prefix, self.config.prefix, True,
-                logfile=self.logfile)
+        if BuildSteps.RELOCATE_OSX_LIBRARIES in self.skip_steps:
+            return
+
         def get_real_path(fp):
             return os.path.realpath(os.path.join(self.config.prefix, fp))
 
@@ -495,6 +500,8 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
             return fp.split('/')[0] in ['lib', 'bin', 'libexec'] and \
                     os.path.splitext(fp)[1] not in ['.a', '.pc', '.la']
 
+        relocator = OSXRelocator(self.config.prefix, self.config.prefix, True,
+                logfile=self.logfile)
         # Only relocate files are that are potentially relocatable and
         # remove duplicates by symbolic links so we relocate libs only
         # once.
@@ -506,6 +513,9 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
         '''
         Codesign OSX build-tools binaries
         '''
+        if BuildSteps.CODE_SIGN in self.skip_steps:
+            return
+
         def get_real_path(fp):
             return os.path.realpath(os.path.join(self.config.prefix, fp))
 
@@ -762,10 +772,7 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
     def steps(self):
         return self._steps
 
-    def _remove_steps(self, steps):
-        self._steps = [x for x in self._steps if x not in steps]
-
-    def get_for_arch (self, arch, name):
+    def get_for_arch(self, arch, name):
         return getattr (self, name)
 
 
