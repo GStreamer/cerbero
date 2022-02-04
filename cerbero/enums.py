cb0f93f4666fe0405a797f83b6a8aad278d4abf4
Tim-Philipp Müller <tim@centricular.com>
2022-02-04 15:18:45 +0000

    Back to development

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -20,7 +20,7 @@ from cerbero.errors import FatalError
 
 
 # Safest place to define this since this file imports very few modules
-CERBERO_VERSION = '1.18.6'
+CERBERO_VERSION = '1.18.6.1'
 
 class Platform:
     ''' Enumeration of supported platforms '''
