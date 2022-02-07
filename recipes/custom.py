c639420f18fb2924bc98bf07b23af5303baab7b0
Tim-Philipp Müller <tim@centricular.com>
2022-02-07 18:47:16 +0000

    Build 1.20 branch again

--- recipes/custom.py
+++ recipes/custom.py
@@ -12,8 +12,8 @@ def running_on_cerbero_ci():
 
 class GStreamer(recipe.Recipe):
     licenses = [License.LGPLv2Plus]
-    version = '1.20.0'
-    tagged_for_release = True
+    version = '1.20.0.1'
+    tagged_for_release = False
 
     # Decide what stype to use
     use_git = True
