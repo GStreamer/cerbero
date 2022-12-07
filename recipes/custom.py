ea7fcae29cc23dfde2195cd6ce876da8a7e33f47
Tim-Philipp Müller <tim@centricular.com>
2022-12-07 11:21:41 +0000

    Build main branch again
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1043>

--- recipes/custom.py
+++ recipes/custom.py
@@ -19,8 +19,8 @@ def running_on_cerbero_ci():
 
 class GStreamer(recipe.Recipe):
     licenses = [License.LGPLv2Plus]
-    version = '1.21.3'
-    tagged_for_release = True
+    version = '1.21.3.1'
+    tagged_for_release = False
 
     # Decide what stype to use
     use_git = True
