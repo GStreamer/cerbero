56c7792b4c19ad94de0f95e942b637049d8ef5dc
Matthew Waters <matthew@centricular.com>
2023-05-31 11:00:29 +1000

    bootstrap/android: download NDK R25c
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1191>

--- cerbero/bootstrap/android.py
+++ cerbero/bootstrap/android.py
@@ -24,12 +24,13 @@ from cerbero.bootstrap.bootstrapper import register_toolchain_bootstrapper
 from cerbero.config import Distro, FatalError
 from cerbero.utils import _, shell
 
-NDK_VERSION = 'r21'
-NDK_BASE_URL = 'https://dl.google.com/android/repository/android-ndk-%s-%s-%s.zip'
+NDK_VERSION = 'r25c'
+NDK_BASE_URL = 'https://dl.google.com/android/repository/android-ndk-%s-%s.zip'
 NDK_CHECKSUMS = {
-    'android-ndk-r21-linux-x86_64.zip': 'b65ea2d5c5b68fb603626adcbcea6e4d12c68eb8a73e373bbb9d23c252fc647b',
-    'android-ndk-r21-darwin-x86_64.zip': 'b82a49ec591d6f283acc7a241a8c56a14788320bf85a3375b5f2309b3b0c9b45',
-    'android-ndk-r21-windows-x86_64.zip': 'faf5a09f78dc7b350b2b77e71031d039191f2af66ac7c99494cd7d5a65e8d147',
+    'android-ndk-r25c-linux.zip': '769ee342ea75f80619d985c2da990c48b3d8eaf45f48783a2d48870d04b46108',
+    # doesn't ship as a zip file anymore
+    'android-ndk-r25c-darwin.dmg': '1856108efde22d502399216c65e2a0f7823bdfac1df69fe6a1f3b71ff7be6ced',
+    'android-ndk-r25c-windows.zip': 'f70093964f6cbbe19268f9876a20f92d3a593db3ad2037baadd25fd8d71e84e2',
 }
 
 class AndroidBootstrapper (BootstrapperBase):
@@ -37,7 +38,7 @@ class AndroidBootstrapper (BootstrapperBase):
     def __init__(self, config, offline, assume_yes):
         super().__init__(config, offline)
         self.prefix = self.config.toolchain_prefix
-        url = NDK_BASE_URL % (NDK_VERSION, self.config.platform, self.config.arch)
+        url = NDK_BASE_URL % (NDK_VERSION, self.config.platform)
         self.fetch_urls.append((url, None, NDK_CHECKSUMS[os.path.basename(url)]))
         self.extract_steps.append((url, True, self.prefix))
 
