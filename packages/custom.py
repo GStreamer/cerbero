97590dcad287107e44fbe336e1c85a000cc2900f
Tim-Philipp Müller <tim@centricular.com>
2024-02-23 18:58:25 +0000

    Build 1.23.90 release
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1382>

--- packages/custom.py
+++ packages/custom.py
@@ -5,7 +5,7 @@ from cerbero.enums import License
 
 class GStreamer:
     url = 'http://gstreamer.freedesktop.org'
-    version = '1.23.2.1'
+    version = '1.23.90'
     vendor = 'GStreamer Project'
     licenses = [License.LGPLv2Plus]
     org = 'org.freedesktop.gstreamer'
