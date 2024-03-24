b8d006cab5ab3da410b112126558492752f318d5
Seungha Yang <seungha@centricular.com>
2024-03-24 22:24:02 +0900

    gst-plugins-bad: Ship d3dshader library
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1427>

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -486,6 +486,7 @@ SOFTWARE LICENSE COMPLIANCE.\n\n'''
             'gstwinrt-1.0': None,
             'gstwebrtcnice-1.0': 'gstreamer-webrtc-nice-1.0',
             'gstdxva-1.0' : None,
+            'gstd3dshader-1.0' : None,
         }
         generated_libs = []
 
