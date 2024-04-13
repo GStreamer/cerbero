6555da503183057ed033f44c414ba2b9d99c6407
Tim-Philipp Müller <tim@centricular.com>
2024-04-13 10:50:47 +0100

    Build 1.24 branch again
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1452>

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -20,7 +20,7 @@ from cerbero.errors import FatalError
 
 
 # Safest place to define this since this file imports very few modules
-CERBERO_VERSION = '1.24.2'
+CERBERO_VERSION = '1.24.2.1'
 
 
 class Platform:
--- packages/custom.py
+++ packages/custom.py
@@ -5,7 +5,7 @@ from cerbero.enums import License
 
 class GStreamer:
     url = 'http://gstreamer.freedesktop.org'
-    version = '1.24.2'
+    version = '1.24.2.1'
     vendor = 'GStreamer Project'
     licenses = [License.LGPLv2Plus]
     org = 'org.freedesktop.gstreamer'
