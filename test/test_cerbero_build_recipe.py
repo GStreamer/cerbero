910149d0f58a4b59535a762e3db1c3f29911743b
Jorge Zapata <jorgeluis.zapata@gmail.com>
2024-05-20 12:43:16 +0200

    Pass the recipe build tests
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1477>

--- test/test_cerbero_build_recipe.py
+++ test/test_cerbero_build_recipe.py
@@ -16,31 +16,35 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+import asyncio
 import unittest
 import os
 
-from cerbero.build import recipe
+from cerbero.build import recipe, build, source
 from cerbero.config import Platform, License, Architecture
 from cerbero.errors import FatalError
 from test.test_common import DummyConfig
-from test.test_build_common import Recipe1
 
 # ruff: noqa: E731
 
 
-class Class1(object):
+class Class1(build.Build):
     test = None
 
     def __init__(self):
         self.test = 'CODEPASS'
 
     class1_method = lambda x: None
-    compile = lambda x: 'CODEPASS'
+
+    async def compile(self):
+        return 'CODEPASS'
 
 
-class Class2(object):
+class Class2(source.Source):
     class2_method = lambda x: None
-    fetch = lambda x: 'CODEPASS'
+
+    async def fetch(self):
+        return 'CODEPASS'
 
 
 class Recipe(recipe.Recipe):
@@ -49,35 +53,31 @@ class Recipe(recipe.Recipe):
 
     name = 'recipe'
     version = '0.0.0'
-    licenses = [License.LGPL]
+    licenses = [License.LGPLv2Plus]
     deps = ['dep1', 'dep2']
     platform_deps = {Platform.LINUX: ['dep3'], Platform.WINDOWS: ['dep4']}
-    post_install = lambda x: 'CODEPASS'
 
     files_libs = ['librecipe-test']
     files_bins = ['recipe-test']
-    licenses_bins = [License.GPL]
+    licenses_bins = [License.GPLv2Plus]
     platform_files_test = {Platform.LINUX: ['test1']}
     platform_licenses_test = {Platform.LINUX: [License.BSD]}
 
+    async def post_install(self):
+        return 'CODEPASS'
+
 
 class Class3(object, metaclass=recipe.MetaUniversalRecipe):
-    def _do_step(self, name):
+    async def _do_step(self, name):
         return name
 
 
-class TestReceiptMetaClass(unittest.TestCase):
+class TestRecipeMetaClass(unittest.TestCase):
     def setUp(self):
         self.config = DummyConfig()
         self.config.local_sources = ''
         self.config.sources = ''
-        self.t = Recipe(self.config)
-
-    def testReceiptBases(self):
-        r = recipe.Recipe(self.config)
-        bases = r.__class__.mro()
-        self.assertTrue(Class1 not in bases)
-        self.assertTrue(Class2 not in bases)
+        self.t = Recipe(self.config, {})
 
     def testReceiptSubclassBases(self):
         bases = self.t.__class__.mro()
@@ -87,20 +87,20 @@ class TestReceiptMetaClass(unittest.TestCase):
     def testFunctions(self):
         self.assertTrue(hasattr(self.t, 'class1_method'))
         self.assertTrue(hasattr(self.t, 'class2_method'))
-        self.assertEqual(self.t.fetch(), 'CODEPASS')
-        self.assertEqual(self.t.compile(), 'CODEPASS')
-        self.assertEqual(self.t.post_install(), 'CODEPASS')
+        self.assertEqual(asyncio.run(self.t.fetch()), 'CODEPASS')
+        self.assertEqual(asyncio.run(self.t.compile()), 'CODEPASS')
+        self.assertEqual(asyncio.run(self.t.post_install()), 'CODEPASS')
 
     def testSubclassesInit(self):
         self.assertEqual(self.t.test, 'CODEPASS')
 
 
-class TestReceipt(unittest.TestCase):
+class TestRecipe(unittest.TestCase):
     def setUp(self):
         self.config = DummyConfig()
         self.config.local_sources = 'path1'
         self.config.sources = 'path2'
-        self.recipe = Recipe(self.config)
+        self.recipe = Recipe(self.config, {})
 
     def testSources(self):
         repo_dir = os.path.join(self.config.local_sources, self.recipe.package_name)
@@ -117,31 +117,23 @@ class TestReceipt(unittest.TestCase):
         self.recipe.config.target_platform = Platform.WINDOWS
         self.assertEqual(['dep1', 'dep2', 'dep4'], self.recipe.list_deps())
 
-    def testRemoveSteps(self):
-        self.recipe._remove_steps(['donotexits'])
-        self.assertTrue(recipe.BuildSteps.FETCH in self.recipe._steps)
-        self.recipe._remove_steps([recipe.BuildSteps.FETCH])
-        self.assertTrue(recipe.BuildSteps.FETCH not in self.recipe._steps)
-        r = Recipe(self.config)
-        self.assertTrue(recipe.BuildSteps.FETCH in r._steps)
-
 
 class TestLicenses(unittest.TestCase):
     def setUp(self):
         self.config = DummyConfig()
         self.config.local_sources = ''
         self.config.sources = ''
-        self.recipe = Recipe(self.config)
+        self.recipe = Recipe(self.config, {})
 
     def testLicenses(self):
-        self.assertEqual(self.recipe.licenses, [License.LGPL])
+        self.assertEqual(self.recipe.licenses, [License.LGPLv2Plus])
 
         licenses_libs = self.recipe.list_licenses_by_categories(['libs'])
-        self.assertEqual(licenses_libs['libs'], [License.LGPL])
-        self.assertEqual(list(licenses_libs.values()), [[License.LGPL]])
+        self.assertEqual(licenses_libs['libs'], [License.LGPLv2Plus])
+        self.assertEqual(list(licenses_libs.values()), [[License.LGPLv2Plus]])
         licenses_bins = self.recipe.list_licenses_by_categories(['bins'])
-        self.assertEqual(licenses_bins['bins'], [License.GPL])
-        self.assertEqual(list(licenses_bins.values()), [[License.GPL]])
+        self.assertEqual(licenses_bins['bins'], [License.GPLv2Plus])
+        self.assertEqual(list(licenses_bins.values()), [[License.GPLv2Plus]])
 
         self.recipe.platform = Platform.LINUX
         self.recipe.config.target_platform = Platform.LINUX
@@ -150,13 +142,13 @@ class TestLicenses(unittest.TestCase):
         self.assertEqual(list(licenses_test.values()), [[License.BSD]])
 
 
-class TestMetaUniveralRecipe(unittest.TestCase):
+class TestMetaUniversalRecipe(unittest.TestCase):
     def testBuildSteps(self):
         obj = Class3()
         for _, step in recipe.BuildSteps():
             self.assertTrue(hasattr(obj, step))
             stepfunc = getattr(obj, step)
-            self.assertEqual(stepfunc(), step)
+            self.assertEqual(asyncio.run(stepfunc()), step)
 
 
 class TestUniversalRecipe(unittest.TestCase):
@@ -165,13 +157,13 @@ class TestUniversalRecipe(unittest.TestCase):
         self.config.target_platform = Platform.LINUX
         self.config_x86 = DummyConfig()
         self.config_x86.target_platform = Platform.LINUX
-        self.config_x86.target_architecture = Architecture.X86
+        self.config_x86.target_arch = Architecture.X86
         self.config_x86_64 = DummyConfig()
         self.config_x86_64.target_platform = Platform.LINUX
-        self.config_x86_64.target_architecture = Architecture.X86_64
-        self.recipe = recipe.UniversalRecipe(self.config)
-        self.recipe_x86 = Recipe1(self.config_x86)
-        self.recipe_x86_64 = Recipe1(self.config_x86_64)
+        self.config_x86_64.target_arch = Architecture.X86_64
+        self.recipe = recipe.UniversalFlatRecipe(self.config)
+        self.recipe_x86 = Recipe(self.config_x86, {})
+        self.recipe_x86_64 = Recipe(self.config_x86_64, {})
 
     def testEmpty(self):
         self.assertTrue(self.recipe.is_empty())
@@ -182,8 +174,8 @@ class TestUniversalRecipe(unittest.TestCase):
     def testProxyRecipe(self):
         self.recipe.add_recipe(self.recipe_x86)
         self.assertEqual(self.recipe.name, self.recipe_x86.name)
-        self.assertEqual(self.recipe.licence, self.recipe_x86.licence)
-        self.assertEqual(self.recipe.uuid, self.recipe_x86.uuid)
+        self.assertEqual(self.recipe.version, self.recipe_x86.version)
+        self.assertEqual(self.recipe.licenses, self.recipe_x86.licenses)
 
     def testAddRecipe(self):
         self.recipe.add_recipe(self.recipe_x86)
@@ -192,7 +184,7 @@ class TestUniversalRecipe(unittest.TestCase):
 
     def testDifferentRecipe(self):
         self.recipe.add_recipe(self.recipe_x86)
-        recipe_test = Recipe1(self.config_x86)
+        recipe_test = Recipe(self.config_x86, {})
         recipe_test.name = 'noname'
         self.assertRaises(FatalError, self.recipe.add_recipe, recipe_test)
 
