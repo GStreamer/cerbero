36f7618f5236f3b89175a1df007132b081e62821
L. E. Segovia <amy@centricular.com>
2024-07-26 06:52:00 +0530

    gst-plugins-rs: Compile gstaws and gstrswebrtc separately
    My 64GB Fedora machine now OOMs/freezes on cross-android-universal
    
    Co-authored-by: Nirbheek Chauhan <nirbheek@centricular.com>
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1530>

--- cerbero/build/build.py
+++ cerbero/build/build.py
@@ -1302,11 +1302,15 @@ class Cargo(Build, ModifyEnvBase):
             return min(determine_num_cargo_jobs(), self.config.num_of_cpus)
         return 1
 
-    def get_cargo_args(self):
+    def get_cargo_args(self, packages=None, features=None):
         args = self.cargo_args[:]
-        if self.cargo_features:
-            args += ['--features=' + ','.join(self.cargo_features)]
-        for package in self.cargo_packages:
+        if packages is None:
+            packages = self.cargo_packages
+        if features is None:
+            features = self.cargo_features
+        if features:
+            args += ['--features=' + ','.join(features)]
+        for package in packages:
             args += ['-p', package]
         return args
 
@@ -1412,7 +1416,7 @@ class CargoC(Cargo):
                 link_args += ['-C', f'link-arg={arg}']
             self.set_env('RUSTFLAGS', ' '.join(link_args))
 
-    def get_cargoc_args(self):
+    def get_cargoc_args(self, packages=None, features=None):
         cargoc_args = [
             '--release',
             '--frozen',
@@ -1432,13 +1436,13 @@ class CargoC(Cargo):
         if self.config.variants.nochecks:
             cargoc_args += ['--config', 'overflow-checks=false']
 
-        cargoc_args += self.get_cargo_args()
+        cargoc_args += self.get_cargo_args(packages, features)
         return cargoc_args
 
     @modify_environment
-    async def compile(self):
+    async def compile(self, packages=None, features=None):
         self.maybe_add_system_libs(step='configure+compile')
-        cmd = [self.cargo, 'cbuild'] + self.get_cargoc_args()
+        cmd = [self.cargo, 'cbuild'] + self.get_cargoc_args(packages, features)
         await self.retry_run(shell.async_call, cmd, self.cargo_dir, logfile=self.logfile, env=self.env)
 
     @modify_environment
