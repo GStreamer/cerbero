b4857c792e4d6af1499229164ebca2696210d872
Maxim P. DEMENTYEV <dememax@hotmail.com>
2024-07-30 15:18:55 +0200

    wix packages: fix annotations for old python
    Current cerbero-uninstalled script limits the python version by:
    
        if not sys.version_info >= (3, 7, 0):
    
    But on 3.8.10 (used in Ubuntu 20.04.6 LTS Focal Fossa) we've got:
    
    # ./cerbero-uninstalled --help
    WARNING: Running as root
    Traceback (most recent call last):
      File "<string>", line 24, in <module>
      File "/root/github/cerbero/cerbero/main.py", line 224, in main
        Main(sys.argv[1:])
      File "/root/github/cerbero/cerbero/main.py", line 46, in __init__
        self.load_commands()
      File "/root/github/cerbero/cerbero/main.py", line 164, in load_commands
        commands.load_commands(subparsers)
      File "/root/github/cerbero/cerbero/commands/__init__.py", line 67, in load_commands
        __import__('cerbero.commands.%s' % name)
      File "/root/github/cerbero/cerbero/commands/package.py", line 26, in <module>
        from cerbero.packages.packager import Packager
      File "/root/github/cerbero/cerbero/packages/packager.py", line 72, in <module>
        from cerbero.packages.windows import wix_on_ninja  # noqa: E402
      File "/root/github/cerbero/cerbero/packages/windows/wix_on_ninja.py", line 19, in <module>
        class Candle(object):
      File "/root/github/cerbero/cerbero/packages/windows/wix_on_ninja.py", line 24, in Candle
        def compile(self, writer: Writer, inputs: list[str], output: str, implicit_outputs=None, implicit_deps=None):
    
    TypeError: 'type' object is not subscriptable
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1536>

--- cerbero/packages/windows/wix_on_ninja.py
+++ cerbero/packages/windows/wix_on_ninja.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2024 L. E. Segovia <amy@centricular.com>
 # SPDX-License-Ref: LGPL-2.1-or-later
 
+from __future__ import annotations
 from pathlib import Path
 import shlex
 import shutil
