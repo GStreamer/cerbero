4825f56f226256842ec54eccf990132d614829c8
L. E. Segovia <amy@centricular.com>
2024-08-26 14:26:34 -0300

    bootstrap: Add packaging module for GLib 2.80+
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1149>

--- cerbero/bootstrap/build_tools.py
+++ cerbero/bootstrap/build_tools.py
@@ -121,7 +121,7 @@ class BuildTools(BootstrapperBase, Fetch):
                 shutil.move(os.path.join(scriptsdir, f), tof)
             os.rmdir(scriptsdir)
         python = os.path.join(self.config.build_tools_prefix, 'bin', 'python')
-        shell.new_call([python, '-m', 'pip', 'install', 'setuptools'])
+        shell.new_call([python, '-m', 'pip', 'install', 'setuptools', 'packaging'])
 
     async def start(self, jobs=0):
         self.setup_venv()
