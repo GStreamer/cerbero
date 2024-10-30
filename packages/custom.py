ba167c40a1c7a8b591798aa6f3a2f3155973e7a7
Tim-Philipp Müller <tim@centricular.com>
2024-10-30 23:46:50 +0000

    Build 1.24 branch again
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1619>

--- packages/custom.py
+++ packages/custom.py
@@ -5,7 +5,7 @@ from cerbero.enums import License
 
 class GStreamer:
     url = 'http://gstreamer.freedesktop.org'
-    version = '1.24.9'
+    version = '1.24.9.1'
     vendor = 'GStreamer Project'
     licenses = [License.LGPLv2Plus]
     org = 'org.freedesktop.gstreamer'
--- recipes/custom.py
+++ recipes/custom.py
@@ -21,8 +21,8 @@ def running_on_cerbero_ci():
 
 class GStreamer(recipe.Recipe):
     licenses = [License.LGPLv2Plus]
-    version = '1.24.9'
-    tagged_for_release = True
+    version = '1.24.9.1'
+    tagged_for_release = False
 
     # Decide what stype to use
     use_git = True
