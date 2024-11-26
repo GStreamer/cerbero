8e42a2e52fa4616eb5fb05e16a784a8b1ed74531
Ruben Gonzalez <rgonzalez@fluendo.com>
2024-11-26 10:11:46 +0100

    recipe: add helper method to allow inherit from a recipe
    It is intended for self-contained projects and test.
    
    Example of inherit:
    
    ```
    original_recipe = import_recipe('recipes/gst-plugins-rs.recipe')
    
    class Recipe(original_recipe):
        def prepare(self):
            self.cargo_packages = [
                'gtk4',
                'rav1e',
                'tracers'
                'rtp',
                'rtsp',
                'webrtc',
                'webrtchttp',
            ]
            super().prepare()
    ```
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1647>

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -1082,3 +1082,19 @@ class UniversalMergedRecipe(BaseUniversalRecipe, UniversalMergedFilesProvider):
         # merge the architecture specific files
         for f, archs in arch_files.items():
             await generator.merge_files([f], [recipe.config.prefix for arch, recipe in archs])
+
+
+def import_recipe(file, class_name='Recipe'):
+    """
+    Mechanism to load a recipe from other file in order to inherit from it
+    @file The path where the .recipe file is
+    @class_name The recipe to return, by default 'Recipe'
+    """
+    upframe = inspect.stack()[1].frame
+    new_globals = upframe.f_globals.copy()
+    new_globals['__file__'] = file
+    exec(open(file).read(), new_globals)
+    recipe_class = new_globals[class_name]
+    # Cerbero checks for the __module__ being 'builtins' in order to load it again
+    recipe_class.__module__ = None
+    return recipe_class
