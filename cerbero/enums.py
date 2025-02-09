22a3d06446dd2a6ba56d072efc1c2be17492eea7
Tim-Philipp Müller <tim@centricular.com>
2025-02-09 20:42:51 +0000

    Build main branch again
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1711>

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -20,7 +20,7 @@ from cerbero.errors import FatalError
 
 
 # Safest place to define this since this file imports very few modules
-CERBERO_VERSION = '1.25.50'
+CERBERO_VERSION = '1.25.50.1'
 
 
 class Platform:
