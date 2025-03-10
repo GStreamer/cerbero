07ed7f4b518e092467ae2cd3c147b957657b7bd0
Ruben Gonzalez <rgonzalez@fluendo.com>
2025-03-10 18:42:19 +0100

    meson: check if Rust variant to configure rustc bin
    Fix Rust Unsupported build platform/arch combination with norust variant
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1759>

--- cerbero/build/build.py
+++ cerbero/build/build.py
@@ -977,7 +977,7 @@ class Meson(Build, ModifyEnvBase):
             binaries['qmake6'] = [self.config.qt6_qmake_path]
 
         # Point meson to rustc with correct arguments to ensure it's detected when cross-compiling
-        if self.config.cargo_home:
+        if self.config.cargo_home and self.config.variants.rust:
             target_triple = self.config.rust_triple(
                 self.config.target_arch, self.config.target_platform, self.using_msvc()
             )
