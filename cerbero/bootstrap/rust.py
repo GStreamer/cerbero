739e33df1734eca08caf02b6047be14ba3f76dbe
Sebastian Dröge <sebastian@centricular.com>
2025-06-30 09:43:02 +0300

    rust: Update to 1.88
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1877>

--- cerbero/bootstrap/rust.py
+++ cerbero/bootstrap/rust.py
@@ -46,7 +46,7 @@ class RustBootstrapper(BootstrapperBase):
 
     SERVER = 'https://static.rust-lang.org'
     RUSTUP_VERSION = '1.28.1'
-    RUST_VERSION = '1.87.0'
+    RUST_VERSION = '1.88.0'
     RUSTUP_URL_TPL = '{server}/rustup/archive/{version}/{triple}/rustup-init{exe_suffix}'
     RUSTUP_NAME_TPL = 'rustup-init-{version}-{triple}{exe_suffix}'
     CHANNEL_URL_TPL = '{server}/dist/channel-rust-{version}.toml'
@@ -55,7 +55,7 @@ class RustBootstrapper(BootstrapperBase):
     TOMLI_URL = 'https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz'
     DOWNLOAD_CHECKSUMS = {
         # Rust packages metadata
-        'channel-rust-1.87.0.toml': '2949b5ea91e3f9c45e75ff2fc6cfc7776616c693ab599ed43abc2120b7522415',
+        'channel-rust-1.88.0.toml': '431b7c5c0b9a511d8e31d29b378bbc74124e8521f14beb92d3a5a5f7e7e55449',
         # Tomli Python module
         'tomli-2.0.1.tar.gz': 'de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f',
         # Rustup
