9157f25fa7d47b2db3b64c1e658959fd65fda0e8
Norbert Hańderek <norbert.handerek03@gmail.com>
2025-10-02 11:37:02 +0200

    cerbero: EndeavourOS support
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/1956>

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -379,7 +379,7 @@ Terminating.""",
         elif d[0].strip() in ['openSUSE Tumbleweed']:
             distro = Distro.SUSE
             distro_version = DistroVersion.OPENSUSE_TUMBLEWEED
-        elif d[0].strip() in ['arch', 'Arch Linux', 'Manjaro Linux']:
+        elif d[0].strip() in ['arch', 'Arch Linux', 'Manjaro Linux', 'EndeavourOS']:
             distro = Distro.ARCH
             distro_version = DistroVersion.ARCH_ROLLING
         elif d[0].strip() in ['Gentoo Base System', 'Gentoo']:
