169fb63b898269cc6c383a5a57065a50f35686d0
Nirbheek Chauhan <nirbheek@centricular.com>
2026-02-19 05:16:12 +0530

    wheels: Reset terminal state on cli exit
    gst-play sets the terminal state such that echoing is disabled, and if
    we ctrl+c it, it doesn't get a chance to reset the terminal state. Do
    it ourselves.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/2120>

--- data/wheel/gstreamer_cli/entrypoints.py
+++ data/wheel/gstreamer_cli/entrypoints.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import shutil
 import subprocess
 import sys
+import os
 
 from gstreamer_libs import gstreamer_env
 
@@ -12,10 +13,20 @@ def __run(program: str):
     if not fullpath:
         raise RuntimeError(f'{program} was not found in {runtime_path}')
     fullpath = str(Path(fullpath).resolve())
+    # Restore terminal state on exit if the called program changed the state,
+    # such as gst-play
+    if os.name == 'posix':
+        import termios
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
     try:
         subprocess.check_call([fullpath, *sys.argv[1:]], env=env)
     except KeyboardInterrupt:
         return 130
     except subprocess.CalledProcessError as e:
         return e.returncode
+    finally:
+        if os.name == 'posix':
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return 0
