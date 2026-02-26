c194e9cc8291e8cee58eed7e9feee533a1a8f38f
Tim-Philipp Müller <tim@centricular.com>
2026-02-26 03:01:26 +0000

    Build 1.28.1 release
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/2139>

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -22,7 +22,7 @@ from cerbero.errors import FatalError
 
 
 # Safest place to define this since this file imports very few modules
-CERBERO_VERSION = '1.28.0.1'
+CERBERO_VERSION = '1.28.1'
 
 
 if sys.version_info >= (3, 11) and 'CI' in os.environ:
