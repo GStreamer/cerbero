d5318ec6cdb6fb38fc5adfae3031bb9f67409868
L. E. Segovia <amy@centricular.com>
2026-06-16 13:12:16 -0300

    cerbero: Reenable deps cache for gst-libav and pkg-config
    These were removed by mistake in !2021, breaking cache for ffmpeg and
    pkg-config.
    
    This partially reverts commit 0a048e62ca108aa3b25fc8f9127141d359fadf9e.
    
    Fixes #583
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/cerbero/-/merge_requests/2269>

--- ci/cerbero_setup.sh
+++ ci/cerbero_setup.sh
@@ -283,7 +283,7 @@ cerbero_deps_script() {
     # plugins (and hence compile against gstreamer)
     local build_deps="gstreamer-1.0 gst-plugins-base-1.0 gst-plugins-good-1.0
         gst-plugins-bad-1.0 gst-plugins-ugly-1.0 gst-rtsp-server-1.0
-        gst-devtools-1.0 gst-editing-services-1.0 libnice"
+        gst-devtools-1.0 gst-editing-services-1.0 libnice gst-libav-1.0"
     local have_rust=`$CERBERO $CERBERO_ARGS show-config | grep 'rust : True'`
     if [[ x$have_rust != x ]]; then
         build_deps="$build_deps gst-plugins-rs"
@@ -296,6 +296,11 @@ cerbero_deps_script() {
     # Deps that don't get picked up automatically because are only listed in
     # the package files
     local more_deps="glib-networking"
+    # Deps that don't get picked up automatically because they are
+    # a runtime dep
+    if [[ $ARCH =~ darwin|msvc|mingw ]]; then
+        more_deps="$more_deps pkg-config"
+    fi
 
     show_ccache_sum
 
